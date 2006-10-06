d9e2a6df4a296b44d8058598d875a0c13271b9ac
Marcos Meli <marcos@filehelpers.com>
2006-10-06 17:51:21 +0000

    ADD: The Great Rodolfo Finochietti's GenericDatabaseStorage.cs
    git-svn-id: svn://localhost/trunk@235 fafefce7-770d-0410-b8bf-a6f642d37c66

---
+++ FileHelpers/DataLink/Storage/GenericDatabaseStorage.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Data;
+
+namespace FileHelpers.DataLink
+{
+    public sealed class GenericDatabaseStorage<T, U> : DatabaseStorage
+        where T : IDbConnection, new( )
+        where U : IDbCommand, new( )
+    {
+        #region Constructors
+
+        public GenericDatabaseStorage( Type recordType, string connectionString ) : base( recordType )
+        {
+            _connectionString = connectionString;
+        }
+
+        #endregion
+
+        #region Properties
+
+        private string _connectionString = String.Empty;
+
+        public string ConnectionString
+        {
+            get
+            {
+                return _connectionString;
+            }
+            set
+            {
+                _connectionString = value;
+            }
+        }
+
+        protected override bool ExecuteInBatch
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        protected sealed override IDbConnection CreateConnection( )
+        {
+            if ( String.IsNullOrEmpty( _connectionString ) )
+                //throw new FileHelperException( "The connection cannot open because connection string is null or empty." );
+                throw new Exception( "The connection cannot open because connection string is null or empty." );
+
+            T connection = new T( );
+            connection.ConnectionString = _connectionString;
+
+            return connection;
+        }
+
+        protected override IDbCommand CreateCommand( )
+        {
+            return new U( );
+        }
+
+        #endregion
+    }
+}
