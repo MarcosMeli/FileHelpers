bf23048d79522921b0a64c8943a846495aedadfa
Marcos Meli <marcos@filehelpers.com>
2007-01-16 22:02:37 +0000

    ADD: a New Performance Project to measure the timing of the library
    git-svn-id: svn://localhost/trunk@287 fafefce7-770d-0410-b8bf-a6f642d37c66

---
+++ FileHelpers.Benchmarks/BenchMark.cs
@@ -0,0 +1,25 @@
+using System;
+using FileHelpers;
+
+namespace FileHelpers.Benchmarks
+{
+	class Benchmark
+	{
+
+		[STAThread]
+		static void Main(string[] args)
+		{
+			FileHelperAsyncEngine engine = new FileHelperAsyncEngine (typeof(TestRecord));
+
+			engine.BeginReadFile(@"E:\_SVN\FileHelpers\test2.csv");
+			//string s;
+			
+			while(engine.ReadNext() != null)
+			{
+				
+			}
+			
+			engine.Close();
+		}
+	}
+}
---
+++ FileHelpers.Benchmarks/TestRecord.cs
@@ -0,0 +1,166 @@
+using System;
+
+namespace FileHelpers.Benchmarks
+{
+
+	[DelimitedRecord(",")]
+	[IgnoreFirst(2)]
+	internal class TestRecord
+	{
+		public string OrderID;
+		public string CustomerID;
+		public string EmployeeID;
+		public string OrderDate;
+		public string RequiredDate;
+		public string ShippedDate;
+		public string ShipVia;
+		public string Freight;
+		public string ShipName;
+		public string ShipAddress;
+		public string ShipCity;
+		public string ShipRegion;
+		public string ShipPostalCode;
+		public string ShipCountry;
+		public string _OrderID;
+		public string ProductID;
+		public string UnitPrice;
+		public string Quantity;
+		public string Discount;
+		public string _ProductID;
+		public string ProductName;
+		public string SupplierID;
+		public string CategoryID;
+		public string QuantityPerUnit;
+		public string UnitPrice_;
+		public string UnitsInStock;
+		public string UnitsOnOrder;
+		public string ReorderLevel;
+		public string Discontinued;
+
+
+		public string OrderID2;
+		public string CustomerID2;
+		public string EmployeeID2;
+		public string OrderDate2;
+		public string RequiredDate2;
+		public string ShippedDate2;
+		public string ShipVia2;
+		public string Freight2;
+		public string ShipName2;
+		public string ShipAddress2;
+		public string ShipCity2;
+		public string ShipRegion2;
+		public string ShipPostalCode2;
+		public string ShipCountry2;
+		public string _OrderID2;
+		public string ProductID2;
+		public string UnitPrice2;
+		public string Quantity2;
+		public string Discount2;
+		public string _ProductID2;
+		public string ProductName2;
+		public string SupplierID2;
+		public string CategoryID2;
+		public string QuantityPerUnit2;
+		public string UnitPrice_2;
+		public string UnitsInStock2;
+		public string UnitsOnOrder2;
+		public string ReorderLevel2;
+		public string Discontinued2;
+
+
+		public string OrderID3;
+		public string CustomerID3;
+		public string EmployeeID3;
+		public string OrderDate3;
+		public string RequiredDate3;
+		public string ShippedDate3;
+		public string ShipVia3;
+		public string Freight3;
+		public string ShipName3;
+		public string ShipAddress3;
+		public string ShipCity3;
+		public string ShipRegion3;
+		public string ShipPostalCode3;
+		public string ShipCountry3;
+		public string _OrderID3;
+		public string ProductID3;
+		public string UnitPrice3;
+		public string Quantity3;
+		public string Discount3;
+		public string _ProductID3;
+		public string ProductName3;
+		public string SupplierID3;
+		public string CategoryID3;
+		public string QuantityPerUnit3;
+		public string UnitPrice_3;
+		public string UnitsInStock3;
+		public string UnitsOnOrder3;
+		public string ReorderLevel3;
+		public string Discontinued3;
+
+
+		public string OrderID4;
+		public string CustomerID4;
+		public string EmployeeID4;
+		public string OrderDate4;
+		public string RequiredDate4;
+		public string ShippedDate4;
+		public string ShipVia4;
+		public string Freight4;
+		public string ShipName4;
+		public string ShipAddress4;
+		public string ShipCity4;
+		public string ShipRegion4;
+		public string ShipPostalCode4;
+		public string ShipCountry4;
+		public string _OrderID4;
+		public string ProductID4;
+		public string UnitPrice4;
+		public string Quantity4;
+		public string Discount4;
+		public string _ProductID4;
+		public string ProductName4;
+		public string SupplierID4;
+		public string CategoryID4;
+		public string QuantityPerUnit4;
+		public string UnitPrice_4;
+		public string UnitsInStock4;
+		public string UnitsOnOrder4;
+		public string ReorderLevel4;
+		public string Discontinued4;
+
+
+		public string OrderID5;
+		public string CustomerID5;
+		public string EmployeeID5;
+		public string OrderDate5;
+		public string RequiredDate5;
+		public string ShippedDate5;
+		public string ShipVia5;
+		public string Freight5;
+		public string ShipName5;
+		public string ShipAddress5;
+		public string ShipCity5;
+		public string ShipRegion5;
+		public string ShipPostalCode5;
+		public string ShipCountry5;
+		public string _OrderID5;
+		public string ProductID5;
+		public string UnitPrice5;
+		public string Quantity5;
+		public string Discount5;
+		public string _ProductID5;
+		public string ProductName5;
+		public string SupplierID5;
+		public string CategoryID5;
+		public string QuantityPerUnit5;
+		public string UnitPrice_5;
+		public string UnitsInStock5;
+		public string UnitsOnOrder5;
+		public string ReorderLevel5;
+		public string Discontinued5; 
+
+		
+	}
+}
