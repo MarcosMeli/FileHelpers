203c9b9a472077a782eb94b6bd1977a8f9566864
Marcos Meli <marcos@filehelpers.com>
2007-03-24 20:49:00 +0000

    MOD: FileMerger.exe and FileReplace.exe (now FileReplace.exe supports wildcard names)
    git-svn-id: svn://localhost/trunk@361 fafefce7-770d-0410-b8bf-a6f642d37c66

--- Libs/FileReplace/Main.cs
+++ Libs/FileReplace/Main.cs
@@ -24,16 +24,49 @@ namespace FileReplace
 				srcString = args[1];
 				newString = args[2];
 
-				System.IO.StreamReader reader = new StreamReader(destFile);
-				string originalStr = reader.ReadToEnd();
-				reader.Close();
+				if (srcString == string.Empty)
+					ShowSyntaxs();
 
-				StreamWriter writer = new StreamWriter(destFile);
-				writer.Write(originalStr.Replace(srcString, newString));
-				writer.Close();
+				if (newString == "-f")
+				{
+					if (args.Length < 4)
+					{
+						ShowSyntaxs();
+						return;
+					}
 
-				Console.WriteLine("Finish to Replace '" + srcString + "' with '" + newString + "'.");
+					System.IO.StreamReader reader = new StreamReader(args[3]);
+					newString = reader.ReadToEnd();
+					reader.Close();
+				
+				}
+
+				foreach (string	file in Directory.GetFiles(Path.GetDirectoryName(destFile), Path.GetFileName(destFile)))
+				{
+					ReplaceFile(file, newString, srcString);
+				}
+
 			}
+		}
+
+		private static void ReplaceFile(string destFile, string newString, string srcString)
+		{
+			System.IO.StreamReader reader = new StreamReader(destFile);
+			string originalStr = reader.ReadToEnd();
+			reader.Close();
+
+			StreamWriter writer = new StreamWriter(destFile);
+			writer.Write(originalStr.Replace(srcString, newString));
+			writer.Close();
+
+
+			Console.WriteLine("Replaced: '" + FirstChars(srcString, 25) + "' --> '" + FirstChars(newString, 25) + "'.");
+		}
+
+		private static string FirstChars(string source, int chars)
+		{
+			return source.Substring(0, Math.Min(chars, source.Length)).Replace(Environment.NewLine, " ");
+			
 		}
 
 		private static void ShowSyntaxs()
@@ -41,6 +74,9 @@ namespace FileReplace
 			Console.WriteLine("Syntaxis:");
 			Console.WriteLine("");
 			Console.WriteLine("FileReplace.exe destFile srcString destString");
+			Console.WriteLine("");
+			Console.WriteLine("   or");
+			Console.WriteLine("FileReplace.exe destFile srcString -f destSourceFile");
 		}
 	}
 }
