6f95ed6f849064b23c682417827f630fda789a9d
Marcos Meli <marcos@filehelpers.com>
2007-03-27 03:03:44 +0000

    ADD: DataMapper a lot of methods to MapDataReader2Records() with a valid DataReader or with a connection and a Select SQL statement.
    This class really simplify the extraction of data from any IDbConnection or a DataTable to the records.
    
    REFLECTION remove pending
    
    git-svn-id: svn://localhost/trunk@369 fafefce7-770d-0410-b8bf-a6f642d37c66

--- FileHelpers/Mapping/DataMapper.cs
+++ FileHelpers/Mapping/DataMapper.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.ComponentModel;
 using System.Data;
 using System.Reflection;
 
@@ -7,7 +8,7 @@ namespace FileHelpers.Mapping
 {
 
 	/// <summary>
-	/// A class to provide Record-DataTable operations.
+	/// A class to provide Record-DataTable operations. (BETA QUALITY, use it at your own risk :P, API can change in future version)
 	/// </summary>
 	public sealed class DataMapper
 	{
@@ -80,11 +81,31 @@ namespace FileHelpers.Mapping
 
 
 		/// <summary>
-		/// Map row to a record.
+		/// Map a source row to a record.
 		/// </summary>
 		/// <param name="dr">The source DataRow</param>
-		/// <returns>The mapped record contained the values DataRow</returns>
+		/// <returns>The mapped record containing the values of the DataRow</returns>
+		[EditorBrowsable(EditorBrowsableState.Advanced)]
 		public object MapRow2Record(DataRow dr)
+		{
+			object record = mRecordInfo.CreateRecordObject();
+			//TypedReference t = TypedReference.MakeTypedReference(record, new FieldInfo[]) null);
+				
+			for(int i = 0; i < mMappings.Count; i++)
+			{
+				((MappingInfo) mMappings[i]).DataToField(dr, record);
+			}
+
+			return record;
+		}
+
+		/// <summary>
+		/// Map a source row to a record.
+		/// </summary>
+		/// <param name="dr">The already opened DataReader</param>
+		/// <returns>The mapped record containing the values of the DataReader</returns>
+		[EditorBrowsable(EditorBrowsableState.Advanced)]
+		public object MapRow2Record(IDataReader dr)
 		{
 			object record = mRecordInfo.CreateRecordObject();
 			//TypedReference t = TypedReference.MakeTypedReference(record, new FieldInfo[]) null);
@@ -109,12 +130,12 @@ namespace FileHelpers.Mapping
 
 			FieldInfo[] fields =
 				mRecordInfo.mRecordType.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.GetField |
-				                                  BindingFlags.Instance | BindingFlags.IgnoreCase);
+				BindingFlags.Instance | BindingFlags.IgnoreCase);
 			
 
 			if (fields.Length > dt.Columns.Count)
 				throw new FileHelperException("The data table has less fields than fields in the Type: " +
-				                              mRecordInfo.mRecordType.Name);
+					mRecordInfo.mRecordType.Name);
 			
 			for(int i = 0; i < fields.Length; i++)
 			{
@@ -140,6 +161,82 @@ namespace FileHelpers.Mapping
 			
 			return (object[]) arr.ToArray(mRecordInfo.mRecordType);
 		}
+
+	
+		/// <summary>
+		/// For each row in the datatable create a record.
+		/// </summary>
+		/// <param name="connection">A valid connection (Opened or not)</param>
+		/// <param name="selectSql">The Sql statement used to return the records.</param>
+		/// <returns>The mapped records contained in the DataTable</returns>
+		public object[] MapDataReader2Records(IDbConnection connection, string selectSql)
+		{
+			ExHelper.CheckNullParam(connection, "connection");
+			ExHelper.CheckNullOrEmpty(selectSql, "selectSql");
+
+			IDbCommand cmd = connection.CreateCommand();
+			cmd.CommandText = selectSql;
+
+			IDataReader dr = null;
+			object[] res;
+			bool connectionOpened = false;
+
+			try
+			{
+				if (connection.State == ConnectionState.Closed)
+				{
+					connection.Open();
+					connectionOpened = true;
+				}
+
+				dr = cmd.ExecuteReader(CommandBehavior.SingleResult);
+				res = MapDataReader2Records(dr);
+			}
+			finally
+			{
+				if (dr != null)
+					dr.Close();
+
+				if (connectionOpened)
+					connection.Close();
+			}
+
+			return res;
+		}
+	
+		/// <summary>
+		/// For each row in the datatable create a record.
+		/// </summary>
+		/// <param name="dr">The source DataReader</param>
+		/// <returns>The mapped records contained in the DataTable</returns>
+		public object[] MapDataReader2Records(IDataReader dr)
+		{
+			ExHelper.CheckNullParam(dr, "dr");
+
+			mMappings.TrimToSize();
+			
+			ArrayList arr = new ArrayList();
+
+			bool hasRows = true;
+			if (dr is System.Data.SqlClient.SqlDataReader)
+				hasRows = ((System.Data.SqlClient.SqlDataReader) dr).HasRows;
+			else if (dr is System.Data.OleDb.OleDbDataReader)
+				hasRows = ((System.Data.OleDb.OleDbDataReader) dr).HasRows;
+			
+
+			if (hasRows)
+			{
+				while (dr.Read())
+				{
+					arr.Add(MapRow2Record(dr));
+				}
+			}
+			
+			return (object[]) arr.ToArray(mRecordInfo.mRecordType);
+		}
+
+	
+	
 	}
 
 	internal class MappingInfo
@@ -186,6 +283,25 @@ namespace FileHelpers.Mapping
 			
 		}
 
+		private void GetColumnIndex(IDataReader dr)
+		{
+			if (mDataColumnIndex == -1)
+			{
+				try
+				{
+					mDataColumnIndex = dr.GetOrdinal(mDataColumnName);
+					if (mDataColumnIndex == -1)
+						throw new FileHelperException("The column : " + mDataColumnName + " was not found in the data reader.");
+				}
+				catch
+				{
+					throw new FileHelperException("The column : " + mDataColumnName + " was not found in the data reader.");
+				}
+				
+			}
+			
+		}
+
 		byte mCheckTypeFlag = 0;
 		Type mColumnType;
 		
@@ -221,6 +337,41 @@ namespace FileHelpers.Mapping
 			}
 			
 		}
+
+	
+		public void DataToField(IDataReader dr, object record) // TypedReference t)
+		{
+			try
+			{
+				GetColumnIndex(dr);
+			
+				if (mCheckTypeFlag == 0)
+				{
+					mColumnType = dr.GetFieldType(mDataColumnIndex);
+					if (mColumnType == mField.FieldType)
+						mCheckTypeFlag = 1;
+					else
+						mCheckTypeFlag = 2;
+				}
+			
+				if (mCheckTypeFlag == 1)
+					mField.SetValue(record, dr[mDataColumnIndex]);
+				else if (dr[mDataColumnIndex] != DBNull.Value)
+					mField.SetValue(record, Convert.ChangeType(dr[mDataColumnIndex], mField.FieldType));
+				else // IS DB NULl
+				{
+					if (mColumnType == typeof(string))
+						mField.SetValue(record, string.Empty);
+				}
+				
+			}
+			catch
+			{
+				throw new FileHelperException(string.Format("Error converting value: {0} to {1} in the column with index {2} and the field {3}", dr[mDataColumnIndex], mField.FieldType, mDataColumnIndex, mField.Name));
+			}
+			
+		}
+
 	}
 	
 }
