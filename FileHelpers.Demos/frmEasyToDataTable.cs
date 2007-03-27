4f47ee1309c39b312491e7e2baeff8adf32b6f24
Marcos Meli <marcos@filehelpers.com>
2007-03-27 03:30:58 +0000

    MOD: FieldSorter faster too, Removed REFLECTION for EMIT
    git-svn-id: svn://localhost/trunk@371 fafefce7-770d-0410-b8bf-a6f642d37c66

--- FileHelpers.Demos/frmEasyToDataTable.cs
+++ FileHelpers.Demos/frmEasyToDataTable.cs
@@ -17,7 +17,7 @@ namespace FileHelpersSamples
 		private Label label3;
 		private System.Windows.Forms.TextBox textBox1;
 		private System.Windows.Forms.Label label4;
-		private System.Windows.Forms.DataGrid dataGrid1;
+		private System.Windows.Forms.DataGrid DataGridDatos;
 
 		/// <summary>
 		/// Required designer variable.
@@ -65,8 +65,8 @@ namespace FileHelpersSamples
 			this.label3 = new System.Windows.Forms.Label();
 			this.textBox1 = new System.Windows.Forms.TextBox();
 			this.label4 = new System.Windows.Forms.Label();
-			this.dataGrid1 = new System.Windows.Forms.DataGrid();
-			((System.ComponentModel.ISupportInitialize)(this.dataGrid1)).BeginInit();
+			this.DataGridDatos = new System.Windows.Forms.DataGrid();
+			((System.ComponentModel.ISupportInitialize)(this.DataGridDatos)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// pictureBox3
@@ -77,12 +77,12 @@ namespace FileHelpersSamples
 			// txtData
 			// 
 			this.txtData.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
-			this.txtData.Location = new System.Drawing.Point(8, 320);
+			this.txtData.Location = new System.Drawing.Point(8, 328);
 			this.txtData.Multiline = true;
 			this.txtData.Name = "txtData";
 			this.txtData.ReadOnly = true;
 			this.txtData.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-			this.txtData.Size = new System.Drawing.Size(664, 144);
+			this.txtData.Size = new System.Drawing.Size(664, 136);
 			this.txtData.TabIndex = 1;
 			this.txtData.Text = @"ALFKI      Alfreds Futterkiste                   Maria Anders          Sales Representative                  Obere Str. 57                            Berlin            Germany   
 ANATR      Ana Trujillo Emparedados y helados    Ana Trujillo          Owner                                 Avda. de la Constitución 2222            México D.F.       Mexico    
@@ -111,7 +111,7 @@ BOLID      Bólido Comidas preparadas             Martín Sommer         Owner    
 			this.label1.BackColor = System.Drawing.Color.Transparent;
 			this.label1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
 			this.label1.ForeColor = System.Drawing.Color.White;
-			this.label1.Location = new System.Drawing.Point(8, 120);
+			this.label1.Location = new System.Drawing.Point(8, 104);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(216, 16);
 			this.label1.TabIndex = 8;
@@ -122,7 +122,7 @@ BOLID      Bólido Comidas preparadas             Martín Sommer         Owner    
 			this.label3.BackColor = System.Drawing.Color.Transparent;
 			this.label3.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
 			this.label3.ForeColor = System.Drawing.Color.White;
-			this.label3.Location = new System.Drawing.Point(8, 304);
+			this.label3.Location = new System.Drawing.Point(8, 312);
 			this.label3.Name = "label3";
 			this.label3.Size = new System.Drawing.Size(264, 16);
 			this.label3.TabIndex = 9;
@@ -135,10 +135,9 @@ BOLID      Bólido Comidas preparadas             Martín Sommer         Owner    
 			this.textBox1.Multiline = true;
 			this.textBox1.Name = "textBox1";
 			this.textBox1.ReadOnly = true;
-			this.textBox1.Size = new System.Drawing.Size(656, 40);
+			this.textBox1.Size = new System.Drawing.Size(656, 24);
 			this.textBox1.TabIndex = 13;
-			this.textBox1.Text = "FileHelperEngine engine = new FileHelperEngine(typeof (CustomersFixed));\r\n ...  =" +
-				" engine.ReadFileAsDT(\"infile.txt\")";
+			this.textBox1.Text = "DataGridDatos.DataSource = engine.ReadFileAsDT(\"infile.txt\")";
 			this.textBox1.WordWrap = false;
 			// 
 			// label4
@@ -152,21 +151,24 @@ BOLID      Bólido Comidas preparadas             Martín Sommer         Owner    
 			this.label4.TabIndex = 12;
 			this.label4.Text = "Code to Read the File";
 			// 
-			// dataGrid1
+			// DataGridDatos
 			// 
-			this.dataGrid1.CaptionVisible = false;
-			this.dataGrid1.DataMember = "";
-			this.dataGrid1.HeaderForeColor = System.Drawing.SystemColors.ControlText;
-			this.dataGrid1.Location = new System.Drawing.Point(8, 136);
-			this.dataGrid1.Name = "dataGrid1";
-			this.dataGrid1.Size = new System.Drawing.Size(664, 168);
-			this.dataGrid1.TabIndex = 14;
+			this.DataGridDatos.CaptionVisible = false;
+			this.DataGridDatos.DataMember = "";
+			this.DataGridDatos.GridLineColor = System.Drawing.Color.FromArgb(((System.Byte)(224)), ((System.Byte)(224)), ((System.Byte)(224)));
+			this.DataGridDatos.HeaderForeColor = System.Drawing.SystemColors.ControlText;
+			this.DataGridDatos.Location = new System.Drawing.Point(8, 120);
+			this.DataGridDatos.Name = "DataGridDatos";
+			this.DataGridDatos.ParentRowsVisible = false;
+			this.DataGridDatos.ReadOnly = true;
+			this.DataGridDatos.Size = new System.Drawing.Size(664, 184);
+			this.DataGridDatos.TabIndex = 14;
 			// 
 			// frmEasyToDataTable
 			// 
 			this.AutoScaleBaseSize = new System.Drawing.Size(5, 14);
 			this.ClientSize = new System.Drawing.Size(680, 496);
-			this.Controls.Add(this.dataGrid1);
+			this.Controls.Add(this.DataGridDatos);
 			this.Controls.Add(this.label4);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.cmdRun);
@@ -182,8 +184,8 @@ BOLID      Bólido Comidas preparadas             Martín Sommer         Owner    
 			this.Controls.SetChildIndex(this.cmdRun, 0);
 			this.Controls.SetChildIndex(this.label1, 0);
 			this.Controls.SetChildIndex(this.label4, 0);
-			this.Controls.SetChildIndex(this.dataGrid1, 0);
-			((System.ComponentModel.ISupportInitialize)(this.dataGrid1)).EndInit();
+			this.Controls.SetChildIndex(this.DataGridDatos, 0);
+			((System.ComponentModel.ISupportInitialize)(this.DataGridDatos)).EndInit();
 			this.ResumeLayout(false);
 
 		}
@@ -193,7 +195,7 @@ BOLID      Bólido Comidas preparadas             Martín Sommer         Owner    
 		private void cmdRun_Click(object sender, EventArgs e)
 		{
 			FileHelperEngine engine = new FileHelperEngine(typeof (CustomersFixed));
-			dataGrid1.DataSource = engine.ReadStringAsDT(txtData.Text);;
+			DataGridDatos.DataSource = engine.ReadStringAsDT(txtData.Text);;
 		}
 	}
 }
