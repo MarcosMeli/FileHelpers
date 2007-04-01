7db3df087531c2665276729dc66f6a2dced38743
Marcos Meli <marcos@filehelpers.com>
2007-04-01 03:04:21 +0000

    MOD: The build script now shows a meanful message when dont found any Visual Studio Installed
    git-svn-id: svn://localhost/trunk@387 fafefce7-770d-0410-b8bf-a6f642d37c66

--- Build/Build.2003.bat
+++ Build/Build.2003.bat
@@ -1,6 +1,5 @@
 @ECHO ---------------------------------
 @ECHO   Building FileHelpers (package)
 @ECHO ---------------------------------
-@cd ..
-@libs\NAnt\NAnt.exe -nologo build2003
+@..\libs\NAnt\NAnt.exe -nologo build2003
 @pause
--- Build/Build.2005.bat
+++ Build/Build.2005.bat
@@ -1,6 +1,5 @@
 @ECHO ---------------------------------
 @ECHO   Building FileHelpers (package)
 @ECHO ---------------------------------
-@cd ..
-@libs\NAnt\NAnt.exe -nologo build2005
+@..\libs\NAnt\NAnt.exe -nologo build2005
 @pause
