f06b672607f81150cd3befd2556f4438998beeda
Marcos Meli <marcos@filehelpers.com>
2007-04-01 05:32:21 +0000

    BIG CHANGES IN THE CONVERTERS =)
    Completely refactored, much more simple and with a lot of error cheking with performance optimizations
    
    git-svn-id: svn://localhost/trunk@388 fafefce7-770d-0410-b8bf-a6f642d37c66

--- FileHelpers/Converters/ConvertHelpers.cs
+++ FileHelpers/Converters/ConvertHelpers.cs
@@ -13,20 +13,13 @@ namespace FileHelpers
 	/// <remarks>Used by the <see cref="FileHelpers.FieldConverterAttribute"/>.</remarks>
 	internal sealed class ConvertHelpers
 	{
+        private const string DefaultDecimalSep = ".";
+
 		#region "  Constructors  "
 
 		// Not allow direct creation
 		private ConvertHelpers()
 		{
-		}
-
-		static CultureInfo ci;
-
-		static ConvertHelpers()
-		{
-			ci = new CultureInfo(CultureInfo.CurrentCulture.LCID);
-			ci.NumberFormat.NumberDecimalSeparator = ".";
-			ci.NumberFormat.NumberGroupSeparator = ",";
 		}
 
 		#endregion
@@ -42,11 +35,13 @@ namespace FileHelpers
 				ci.NumberFormat.NumberDecimalSeparator = ".";
 				ci.NumberFormat.NumberGroupSeparator = ",";
 			}
-			else
+			else if (decimalSep == ",")
 			{
-				ci.NumberFormat.NumberDecimalSeparator = decimalSep.ToString();
+				ci.NumberFormat.NumberDecimalSeparator = ",";
 				ci.NumberFormat.NumberGroupSeparator = ".";
 			}
+            else
+                throw new BadUsageException("You can only use '.' or ',' as decimal or grup separators");
 
 			return ci;
 		}
@@ -68,33 +63,37 @@ namespace FileHelpers
 
 #endif
 			// Try to assign a default Converter
-			if (fieldType == typeof (Int16))
-				return ToInt16();
-			else if (fieldType == typeof (Int32))
-				return ToInt32();
-			else if (fieldType == typeof (Int64))
-				return ToInt64();
-			else if (fieldType == typeof (SByte))
-				return ToSByte();
-			else if (fieldType == typeof (Decimal))
-				return ToDecimal();
-			else if (fieldType == typeof (Double))
-				return ToDouble();
-			else if (fieldType == typeof (Single))
-				return ToSingle();
-			else if (fieldType == typeof (DateTime))
-				return ToDateTime();
-			else if (fieldType == typeof (Boolean))
-				return ToBoolean();
-			else if (fieldType == typeof (UInt16) ||
-				fieldType == typeof (UInt32) ||
-				fieldType == typeof (UInt64) ||
-				fieldType == typeof (Byte) ||
-				fieldType == typeof (string))
-				return null;
+            if (fieldType == typeof(string))
+                return null;
+            else if (fieldType == typeof(Int16))
+                return new Int16Converter();
+            else if (fieldType == typeof(Int32))
+                return new Int32Converter();
+            else if (fieldType == typeof(Int64))
+                return new Int64Converter();
+            else if (fieldType == typeof(SByte))
+                return new SByteConverter();
+            else if (fieldType == typeof(UInt16))
+                return new UInt16Converter();
+            else if (fieldType == typeof(UInt32))
+                return new UInt32Converter();
+            else if (fieldType == typeof(UInt64))
+                return new UInt64Converter();
+            else if (fieldType == typeof(Byte))
+                return new ByteConverter();
+            else if (fieldType == typeof(Decimal))
+                return new DecimalConverter();
+            else if (fieldType == typeof(Double))
+                return new DoubleConverter();
+            else if (fieldType == typeof(Single))
+                return new SingleConverter();
+            else if (fieldType == typeof(DateTime))
+                return new DateTimeConverter();
+            else if (fieldType == typeof(Boolean))
+                return new BooleanConverter();
 #if ! MINI
-			else if (fieldType.IsEnum)
-				return new EnumConverter(fieldType);
+            else if (fieldType.IsEnum)
+                return new EnumConverter(fieldType);
 #endif			
 
 			throw new BadUsageException("The field: '" + fieldName + "' of type: " + fieldType.Name + " is a non system type, so this field need a CustomConverter (see the docs for more info).");
@@ -102,142 +101,177 @@ namespace FileHelpers
 
 		#endregion
 
+        internal abstract class CultureConverter
+            : ConverterBase 
+        {
+            protected CultureInfo mCulture;
+            protected Type mType;
+            public CultureConverter(Type T, string decimalSep)
+            {
+                mCulture = CreateCulture(decimalSep);
+                mType = T;
+            }
+
+            public sealed override string FieldToString(object from)
+            {
+                return ((IConvertible)from).ToString(mCulture);
+            }
+
+            public sealed override object StringToField(string from)
+            {
+                object val;
+                
+                try
+                {
+                    val = ParseString(from);
+                }
+                catch
+                {
+                    throw new ConvertException(from, mType);
+                }
+
+                return val;
+            }
+
+            protected abstract object ParseString(string from);
+
+        }
+
+        internal sealed class ByteConverter : CultureConverter
+        {
+            public ByteConverter()
+                : this(DefaultDecimalSep)
+            {}
+
+            public ByteConverter(string decimalSep)
+                : base(typeof(Byte), decimalSep)
+            {}
+
+            protected override object ParseString(string from)
+            {
+                return Byte.Parse(StringHelper.RemoveBlanks(from), NumberStyles.Number, mCulture);
+            }
+        }
+
+
+        internal sealed class UInt16Converter : CultureConverter
+        {
+            public UInt16Converter()
+                : this(DefaultDecimalSep)
+            {}
+
+            public UInt16Converter(string decimalSep)
+                : base(typeof(UInt16), decimalSep)
+            { }
+
+            protected override object ParseString(string from)
+            {
+                return UInt16.Parse(StringHelper.RemoveBlanks(from), NumberStyles.Number, mCulture);
+            }
+        }
+
+
+        internal sealed class UInt32Converter : CultureConverter
+        {
+            public UInt32Converter()
+                : this(DefaultDecimalSep)
+            {}
+
+            public UInt32Converter(string decimalSep)
+                : base(typeof(UInt32), decimalSep)
+            { }
+
+            protected override object ParseString(string from)
+            {
+                return UInt32.Parse(StringHelper.RemoveBlanks(from), NumberStyles.Number, mCulture);
+            }
+        }
+
+
+
+        internal sealed class UInt64Converter : CultureConverter
+        {
+            public UInt64Converter()
+                : this(DefaultDecimalSep)
+            {}
+
+            public UInt64Converter(string decimalSep)
+                : base(typeof(UInt64), decimalSep)
+            { }
+
+            protected override object ParseString(string from)
+            {
+                return UInt64.Parse(StringHelper.RemoveBlanks(from), NumberStyles.Number, mCulture);
+            }
+        }
+
 		#region "  Int16, Int32, Int64 Converters  "
-
-		#region "  Convert Handlers  "
-
-		/// <summary>
-		/// Returns a Byte <see cref="ConverterBase">Converter</see>.
-		/// </summary>
-		/// <returns>The <see cref="ConverterBase"/> that performs the convertion.</returns>
-		public static ConverterBase ToSByte()
-		{
-			return new SByteConverter();
-		}
-
-		/// <summary>
-		/// Returns a Int16 <see cref="ConverterBase">Converter</see>.
-		/// </summary>
-		/// <returns>The <see cref="ConverterBase"/> that performs the convertion.</returns>
-		public static ConverterBase ToInt16()
-		{
-			return new Int16Converter();
-		}
-
-		/// <summary>
-		/// Returns a Int32 <see cref="ConverterBase">Converter</see>.
-		/// </summary>
-		/// <returns>The <see cref="ConverterBase"/> that performs the convertion.</returns>
-		public static ConverterBase ToInt32()
-		{
-			return new Int32Converter();
-		}
-
-		/// <summary>
-		/// Returns a Int64 <see cref="ConverterBase">Converter</see>.
-		/// </summary>
-		/// <returns>The <see cref="ConverterBase"/> that performs the convertion.</returns>
-		public static ConverterBase ToInt64()
-		{
-			return new Int64Converter();
-		}
-
-		#endregion 
 
 		#region "  Convert Classes  "
 
-		internal sealed class SByteConverter : ConverterBase
+        internal sealed class SByteConverter : CultureConverter
+        {
+            public SByteConverter()
+                : this(DefaultDecimalSep)
+            {}
+
+            public SByteConverter(string decimalSep)
+                : base(typeof(SByte), decimalSep)
+            { }
+
+            protected override object ParseString(string from)
+            {
+                return SByte.Parse(StringHelper.RemoveBlanks(from), NumberStyles.Number, mCulture);
+            }
+        }
+
+        internal sealed class Int16Converter : CultureConverter
+        {
+            public Int16Converter()
+                : this(DefaultDecimalSep)
+            {}
+
+            public Int16Converter(string decimalSep)
+                : base(typeof(Int16), decimalSep)
+            { }
+
+            protected override object ParseString(string from)
+            {
+                return Int16.Parse(StringHelper.RemoveBlanks(from), NumberStyles.Number, mCulture);
+            }
+        }
+
+        internal sealed class Int32Converter : CultureConverter
+        {
+            public Int32Converter()
+                : this(DefaultDecimalSep)
+            {}
+
+
+            public Int32Converter(string decimalSep)
+                : base(typeof(Int32), decimalSep)
+            { }
+
+            protected override object ParseString(string from)
+            {
+                return Int32.Parse(StringHelper.RemoveBlanks(from), NumberStyles.Number, mCulture);
+            }
+        }
+
+		internal sealed class Int64Converter : CultureConverter
 		{
-			public override object StringToField(string from)
-			{
-				object val;
-				try
-				{
-					val = sbyte.Parse(StringHelper.RemoveBlanks(from));
-				}
-				catch
-				{
-					throw new ConvertException(from, typeof (byte));
-				}
+            public Int64Converter()
+                : this(DefaultDecimalSep)
+            {}
+            public Int64Converter(string decimalSep)
+                   : base(typeof(Int64), decimalSep)
+            { }
 
-				return val;
-			}
-
-			public override string FieldToString(object from)
-			{
-				return Convert.ChangeType(from, typeof (byte), null).ToString();
-			}
-		}
-
-		internal sealed class Int16Converter : ConverterBase
-		{
-			public override object StringToField(string from)
-			{
-				object val;
-				try
-				{
-					val = Int16.Parse(StringHelper.RemoveBlanks(from));
-				}
-				catch
-				{
-					throw new ConvertException(from, typeof (Int16));
-				}
-
-				return val;
-			}
-
-			public override string FieldToString(object from)
-			{
-				return Convert.ChangeType(from, typeof (Int16), null).ToString();
-			}
-		}
-
-		internal sealed class Int32Converter : ConverterBase
-		{
-			public override object StringToField(string from)
-			{
-				object val;
-				try
-				{
-					val = Int32.Parse(StringHelper.RemoveBlanks(from), null);
-				}
-				catch
-				{
-					throw new ConvertException(from, typeof (Int32));
-				}
-
-				return val;
-			}
-
-			public override string FieldToString(object from)
-			{
-				return Convert.ToInt32(from).ToString((IFormatProvider) null);
-			}
-		}
-
-
-		internal sealed class Int64Converter : ConverterBase
-		{
-			public override object StringToField(string from)
-			{
-				object val;
-				try
-				{
-					val = Int64.Parse(StringHelper.RemoveBlanks(from));
-				}
-				catch
-				{
-					throw new ConvertException(from, typeof (Int64));
-				}
-
-				return val;
-			}
-
-			public override string FieldToString(object from)
-			{
-				return Convert.ToInt64(from).ToString((IFormatProvider) null);
-			}
-		}
+            protected override object ParseString(string from)
+            {
+                return Int64.Parse(StringHelper.RemoveBlanks(from), NumberStyles.Number, mCulture);
+            }
+        }
 
 		#endregion
 
@@ -245,146 +279,57 @@ namespace FileHelpers
 
 		#region "  Single, Double, DecimalConverters  "
 
-		#region "  Convert Handlers  "
-
-		/// <summary>
-		/// Returns a Int16 <see cref="ConverterBase">Converter</see>.
-		/// </summary>
-		/// <returns>The <see cref="ConverterBase"/> that performs the convertion.</returns>
-		public static ConverterBase ToDecimal()
-		{
-			return ToDecimal(".");
-		}
-
-		public static ConverterBase ToDecimal(string decimalSeparator)
-		{
-			return new DecimalConverter(decimalSeparator);
-		}
-
-		/// <summary>
-		/// Returns a Int32 <see cref="ConverterBase">Converter</see>.
-		/// </summary>
-		/// <returns>The <see cref="ConverterBase"/> that performs the convertion.</returns>
-		public static ConverterBase ToSingle()
-		{
-			return ToSingle(".");
-		}
-
-		public static ConverterBase ToSingle(string decimalSeparator)
-		{
-			return new SingleConverter(decimalSeparator);
-		}
-
-		/// <summary>
-		/// Returns a Int64 <see cref="ConverterBase">Converter</see>.
-		/// </summary>
-		/// <returns>The <see cref="ConverterBase"/> that performs the convertion.</returns>
-		public static ConverterBase ToDouble()
-		{
-			return ToDouble(".");
-		}
-
-		public static ConverterBase ToDouble(string decimalSeparator)
-		{
-			return new DoubleConverter(decimalSeparator);
-		}
-
-		#endregion 
-
 		#region "  Convert Classes  "
 
-		internal sealed class DecimalConverter : ConverterBase
+		internal sealed class DecimalConverter : CultureConverter
 		{
-			CultureInfo ci;
-
+            public DecimalConverter()
+                : this(DefaultDecimalSep)
+            {}
 			public DecimalConverter(string decimalSep)
+                :base (typeof(Decimal), decimalSep)
 			{
-				ci = CreateCulture(decimalSep);
 			}
 
-			public override object StringToField(string from)
-			{
-				object val;
-				try
-				{
-					val = Decimal.Parse(StringHelper.RemoveBlanks(from), ci);
-				}
-				catch
-				{
-					throw new ConvertException(from, typeof (Decimal));
-				}
+            protected override object ParseString(string from)
+            {
+                return Decimal.Parse(StringHelper.RemoveBlanks(from), mCulture);
+            }
+        }
 
-				return val;
-			}
-
-			public override string FieldToString(object from)
-			{
-				return Convert.ToDecimal(from).ToString(ci);
-			}
-		}
-
-		internal sealed class SingleConverter : ConverterBase
+        internal sealed class SingleConverter : CultureConverter
 		{
-			CultureInfo ci;
+            public SingleConverter()
+                : this(DefaultDecimalSep)
+            {}
 
-
-			
 			public SingleConverter(string decimalSep)
-			{
-				ci = CreateCulture(decimalSep);
-			}
+                : base(typeof(Single), decimalSep)
+			{}
 
-			public override object StringToField(string from)
-			{
-				object val;
-				try
-				{
-					val = Single.Parse(StringHelper.RemoveBlanks(from), ci);
-				}
-				catch
-				{
-					throw new ConvertException(from, typeof (Single));
-				}
-
-				return val;
-			}
-
-			public override string FieldToString(object from)
-			{
-				return Convert.ToSingle(from).ToString(ci);
-			}
-		}
+			protected override object ParseString(string from)
+            {
+                return Single.Parse(StringHelper.RemoveBlanks(from), mCulture);
+            }
+        }
 
 
-		internal sealed class DoubleConverter : ConverterBase
+        internal sealed class DoubleConverter : CultureConverter
 		{
-			CultureInfo ci;
+            public DoubleConverter()
+                : this(DefaultDecimalSep)
+            {}
 
 			public DoubleConverter(string decimalSep)
+                :base(typeof(Double), decimalSep)
 			{
-				ci = CreateCulture(decimalSep);
 			}
 
-			public override object StringToField(string from)
-			{
-				object val;
-				try
-				{
-					val = Double.Parse(StringHelper.RemoveBlanks(from), ci);
-				}
-				catch
-				{
-					throw new ConvertException(from, typeof (Double));
-				}
-
-				return val;
-			}
-
-			public override string FieldToString(object from)
-			{
-				return Convert.ToDouble(from).ToString(ci);
-			}
-		}
+            protected override object ParseString(string from)
+            {
+                return Double.Parse(StringHelper.RemoveBlanks(from), mCulture); 
+            }
+        }
 
 		#endregion
 
@@ -392,30 +337,6 @@ namespace FileHelpers
 
 		#region "  Date Converters  "
 
-		#region "  Convert Handlers  "
-
-		/// <summary>
-		/// <para>Returns a Date <see cref="ConverterBase">Converter</see>.</para>
-		/// <para>Uses the default format "ddMMyyyy".</para>
-		/// </summary>
-		/// <returns>The <see cref="ConverterBase"/> that performs the convertion.</returns>
-		public static ConverterBase ToDateTime()
-		{
-			return ToDateTime("ddMMyyyy");
-		}
-
-		/// <summary>
-		/// <para>Returns a Date <see cref="ConverterBase">Converter</see>.</para>
-		/// <para>Uses the specified format.</para>
-		/// </summary>
-		/// <param name="format">The format used to convert from/to a string.</param>
-		/// <returns>The <see cref="ConverterBase"/> that performs the convertion.</returns>
-		public static ConverterBase ToDateTime(string format)
-		{
-			return new DateTimeConverter(format);
-		}
-
-		#endregion 
 
 		#region "  Convert Classes  "
 
@@ -482,19 +403,6 @@ namespace FileHelpers
 		#endregion
 
 		#region "  Boolean Converters  "
-
-		#region "  Convert Handlers  "
-
-		/// <summary>
-		/// Returns a Boolean <see cref="ConverterBase">Converter</see>.
-		/// </summary>
-		/// <returns>The <see cref="ConverterBase"/> that performs the convertion.</returns>
-		public static ConverterBase ToBoolean()
-		{
-			return new BooleanConverter();
-		}
-
-		#endregion
 
 		#region "  Convert Classes  "
 
