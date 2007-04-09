24cfd0700ed6a466dee12500b86acdf2c8c776c8
Marcos Meli <marcos@filehelpers.com>
2007-04-09 04:02:05 +0000

    MOD: Samples
    git-svn-id: svn://localhost/trunk@441 fafefce7-770d-0410-b8bf-a6f642d37c66

--- FileHelpers.Demos/frmEasySampleAsync.cs
+++ FileHelpers.Demos/frmEasySampleAsync.cs
@@ -61,146 +61,128 @@ namespace FileHelpersSamples
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.txtClass = new System.Windows.Forms.TextBox();
-			this.cmdRun = new System.Windows.Forms.Button();
-			this.label2 = new System.Windows.Forms.Label();
-			this.label1 = new System.Windows.Forms.Label();
-			this.label4 = new System.Windows.Forms.Label();
-			this.textBox1 = new System.Windows.Forms.TextBox();
-			this.txtOut = new System.Windows.Forms.TextBox();
-			this.SuspendLayout();
-			// 
-			// pictureBox3
-			// 
-			this.pictureBox3.Location = new System.Drawing.Point(576, 8);
-			this.pictureBox3.Name = "pictureBox3";
-			// 
-			// txtClass
-			// 
-			this.txtClass.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
-			this.txtClass.Location = new System.Drawing.Point(8, 288);
-			this.txtClass.Multiline = true;
-			this.txtClass.Name = "txtClass";
-			this.txtClass.ReadOnly = true;
-			this.txtClass.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-			this.txtClass.Size = new System.Drawing.Size(328, 160);
-			this.txtClass.TabIndex = 0;
-			this.txtClass.Text = @"	[DelimitedRecord(""|"")]
-	public class CustomersVerticalBar
-	{
-		public string CustomerID;
-		public string CompanyName;
-		public string ContactName;
-		public string ContactTitle;
-		public string Address;
-		public string City;
-		public string Country;
-	}";
-			this.txtClass.WordWrap = false;
-			// 
-			// cmdRun
-			// 
-			this.cmdRun.BackColor = System.Drawing.Color.FromArgb(((System.Byte)(0)), ((System.Byte)(0)), ((System.Byte)(110)));
-			this.cmdRun.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
-			this.cmdRun.ForeColor = System.Drawing.Color.White;
-			this.cmdRun.Location = new System.Drawing.Point(336, 8);
-			this.cmdRun.Name = "cmdRun";
-			this.cmdRun.Size = new System.Drawing.Size(152, 32);
-			this.cmdRun.TabIndex = 0;
-			this.cmdRun.Text = "RUN >>";
-			this.cmdRun.Click += new System.EventHandler(this.cmdRun_Click);
-			// 
-			// label2
-			// 
-			this.label2.BackColor = System.Drawing.Color.Transparent;
-			this.label2.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
-			this.label2.ForeColor = System.Drawing.Color.White;
-			this.label2.Location = new System.Drawing.Point(8, 272);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(216, 16);
-			this.label2.TabIndex = 7;
-			this.label2.Text = "Code of the Mapping Class";
-			// 
-			// label1
-			// 
-			this.label1.BackColor = System.Drawing.Color.Transparent;
-			this.label1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
-			this.label1.ForeColor = System.Drawing.Color.White;
-			this.label1.Location = new System.Drawing.Point(344, 272);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(216, 16);
-			this.label1.TabIndex = 8;
-			this.label1.Text = "Output Array";
-			// 
-			// label4
-			// 
-			this.label4.BackColor = System.Drawing.Color.Transparent;
-			this.label4.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
-			this.label4.ForeColor = System.Drawing.Color.White;
-			this.label4.Location = new System.Drawing.Point(8, 64);
-			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(152, 16);
-			this.label4.TabIndex = 10;
-			this.label4.Text = "Code to Read the File";
-			// 
-			// textBox1
-			// 
-			this.textBox1.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
-			this.textBox1.Location = new System.Drawing.Point(8, 80);
-			this.textBox1.Multiline = true;
-			this.textBox1.Name = "textBox1";
-			this.textBox1.ReadOnly = true;
-			this.textBox1.Size = new System.Drawing.Size(656, 184);
-			this.textBox1.TabIndex = 11;
-			this.textBox1.Text = @"  FileHelperAsyncEngine engine = new FileHelperAsyncEngine(typeof(CustomersVerticalBar));
-
-  engine.BeginReadFile(""TestIn.txt"");
-
-  // The Async engines are IEnumerable
-  foreach(CustomersVerticalBar cust in engine)
-  {
-     // your code here
-     Console.WriteLine(cust.CustomerID + "" - "" + cust.ContactTitle + Environment.NewLine);
-  }
-
-  engine.Close();";
-			this.textBox1.WordWrap = false;
-			// 
-			// txtOut
-			// 
-			this.txtOut.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
-			this.txtOut.Location = new System.Drawing.Point(344, 288);
-			this.txtOut.Multiline = true;
-			this.txtOut.Name = "txtOut";
-			this.txtOut.ReadOnly = true;
-			this.txtOut.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-			this.txtOut.Size = new System.Drawing.Size(328, 160);
-			this.txtOut.TabIndex = 12;
-			this.txtOut.Text = "";
-			this.txtOut.WordWrap = false;
-			// 
-			// frmEasySampleAsync
-			// 
-			this.AutoScaleBaseSize = new System.Drawing.Size(5, 14);
-			this.ClientSize = new System.Drawing.Size(680, 480);
-			this.Controls.Add(this.txtOut);
-			this.Controls.Add(this.label4);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.label2);
-			this.Controls.Add(this.cmdRun);
-			this.Controls.Add(this.txtClass);
-			this.Controls.Add(this.textBox1);
-			this.Name = "frmEasySampleAsync";
-			this.Text = "FileHelpers - Easy Example";
-			this.Controls.SetChildIndex(this.textBox1, 0);
-			this.Controls.SetChildIndex(this.pictureBox3, 0);
-			this.Controls.SetChildIndex(this.txtClass, 0);
-			this.Controls.SetChildIndex(this.cmdRun, 0);
-			this.Controls.SetChildIndex(this.label2, 0);
-			this.Controls.SetChildIndex(this.label1, 0);
-			this.Controls.SetChildIndex(this.label4, 0);
-			this.Controls.SetChildIndex(this.txtOut, 0);
-			this.ResumeLayout(false);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmEasySampleAsync));
+            this.txtClass = new System.Windows.Forms.TextBox();
+            this.cmdRun = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtOut = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(576, 8);
+            // 
+            // txtClass
+            // 
+            this.txtClass.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtClass.Location = new System.Drawing.Point(8, 288);
+            this.txtClass.Multiline = true;
+            this.txtClass.Name = "txtClass";
+            this.txtClass.ReadOnly = true;
+            this.txtClass.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtClass.Size = new System.Drawing.Size(328, 160);
+            this.txtClass.TabIndex = 0;
+            this.txtClass.Text = resources.GetString("txtClass.Text");
+            this.txtClass.WordWrap = false;
+            // 
+            // cmdRun
+            // 
+            this.cmdRun.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(110)))));
+            this.cmdRun.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdRun.ForeColor = System.Drawing.Color.White;
+            this.cmdRun.Location = new System.Drawing.Point(336, 8);
+            this.cmdRun.Name = "cmdRun";
+            this.cmdRun.Size = new System.Drawing.Size(152, 32);
+            this.cmdRun.TabIndex = 0;
+            this.cmdRun.Text = "RUN >>";
+            this.cmdRun.UseVisualStyleBackColor = false;
+            this.cmdRun.Click += new System.EventHandler(this.cmdRun_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(8, 272);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(216, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Code of the Mapping Class";
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(344, 272);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Console Output";
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(8, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(152, 16);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Code to Read the File";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(8, 80);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(656, 184);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            this.textBox1.WordWrap = false;
+            // 
+            // txtOut
+            // 
+            this.txtOut.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOut.Location = new System.Drawing.Point(344, 288);
+            this.txtOut.Multiline = true;
+            this.txtOut.Name = "txtOut";
+            this.txtOut.ReadOnly = true;
+            this.txtOut.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOut.Size = new System.Drawing.Size(328, 160);
+            this.txtOut.TabIndex = 12;
+            this.txtOut.WordWrap = false;
+            // 
+            // frmEasySampleAsync
+            // 
+            this.AutoScaleBaseSize = new System.Drawing.Size(5, 14);
+            this.ClientSize = new System.Drawing.Size(680, 480);
+            this.Controls.Add(this.txtOut);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmdRun);
+            this.Controls.Add(this.txtClass);
+            this.Controls.Add(this.textBox1);
+            this.Name = "frmEasySampleAsync";
+            this.Text = "FileHelpers - Easy Example";
+            this.Controls.SetChildIndex(this.textBox1, 0);
+            this.Controls.SetChildIndex(this.pictureBox3, 0);
+            this.Controls.SetChildIndex(this.txtClass, 0);
+            this.Controls.SetChildIndex(this.cmdRun, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.label4, 0);
+            this.Controls.SetChildIndex(this.txtOut, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
--- FileHelpers.Demos/frmEasyToDataTable.cs
+++ FileHelpers.Demos/frmEasyToDataTable.cs
@@ -59,134 +59,132 @@ namespace FileHelpersSamples
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.txtData = new System.Windows.Forms.TextBox();
-			this.cmdRun = new System.Windows.Forms.Button();
-			this.label1 = new System.Windows.Forms.Label();
-			this.label3 = new System.Windows.Forms.Label();
-			this.textBox1 = new System.Windows.Forms.TextBox();
-			this.label4 = new System.Windows.Forms.Label();
-			this.DataGridDatos = new System.Windows.Forms.DataGrid();
-			((System.ComponentModel.ISupportInitialize)(this.DataGridDatos)).BeginInit();
-			this.SuspendLayout();
-			// 
-			// pictureBox3
-			// 
-			this.pictureBox3.Location = new System.Drawing.Point(578, 7);
-			this.pictureBox3.Name = "pictureBox3";
-			// 
-			// txtData
-			// 
-			this.txtData.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
-			this.txtData.Location = new System.Drawing.Point(8, 328);
-			this.txtData.Multiline = true;
-			this.txtData.Name = "txtData";
-			this.txtData.ReadOnly = true;
-			this.txtData.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-			this.txtData.Size = new System.Drawing.Size(664, 136);
-			this.txtData.TabIndex = 1;
-			this.txtData.Text = @"ALFKI      Alfreds Futterkiste                   Maria Anders          Sales Representative                  Obere Str. 57                            Berlin            Germany   
-ANATR      Ana Trujillo Emparedados y helados    Ana Trujillo          Owner                                 Avda. de la Constitución 2222            México D.F.       Mexico    
-ANTON      Antonio Moreno Taquería               Antonio Moreno        Owner                                 Mataderos  2312                          México D.F.       Mexico    
-AROUT      Around the Horn                       Thomas Hardy          Sales Representative                  120 Hanover Sq.                          London            UK        
-BERGS      Berglunds snabbköp                    Christina Berglund    Order Administrator                   Berguvsvägen  8                          Luleå             Sweden    
-BLAUS      Blauer See Delikatessen               Hanna Moos            Sales Representative                  Forsterstr. 57                           Mannheim          Germany   
-BLONP      Blondesddsl père et fils              Frédérique Citeaux    Marketing Manager                     24, place Kléber                         Strasbourg        France    
-BOLID      Bólido Comidas preparadas             Martín Sommer         Owner                                 C/ Araquil, 67                           Madrid            Spain     ";
-			this.txtData.WordWrap = false;
-			// 
-			// cmdRun
-			// 
-			this.cmdRun.BackColor = System.Drawing.Color.FromArgb(((System.Byte)(0)), ((System.Byte)(0)), ((System.Byte)(110)));
-			this.cmdRun.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
-			this.cmdRun.ForeColor = System.Drawing.Color.Gainsboro;
-			this.cmdRun.Location = new System.Drawing.Point(336, 8);
-			this.cmdRun.Name = "cmdRun";
-			this.cmdRun.Size = new System.Drawing.Size(152, 32);
-			this.cmdRun.TabIndex = 0;
-			this.cmdRun.Text = "RUN >>";
-			this.cmdRun.Click += new System.EventHandler(this.cmdRun_Click);
-			// 
-			// label1
-			// 
-			this.label1.BackColor = System.Drawing.Color.Transparent;
-			this.label1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
-			this.label1.ForeColor = System.Drawing.Color.White;
-			this.label1.Location = new System.Drawing.Point(8, 104);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(216, 16);
-			this.label1.TabIndex = 8;
-			this.label1.Text = "Output DataTable";
-			// 
-			// label3
-			// 
-			this.label3.BackColor = System.Drawing.Color.Transparent;
-			this.label3.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
-			this.label3.ForeColor = System.Drawing.Color.White;
-			this.label3.Location = new System.Drawing.Point(8, 312);
-			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(264, 16);
-			this.label3.TabIndex = 9;
-			this.label3.Text = "Input Data to the FileHelperEngine";
-			// 
-			// textBox1
-			// 
-			this.textBox1.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
-			this.textBox1.Location = new System.Drawing.Point(8, 72);
-			this.textBox1.Multiline = true;
-			this.textBox1.Name = "textBox1";
-			this.textBox1.ReadOnly = true;
-			this.textBox1.Size = new System.Drawing.Size(656, 24);
-			this.textBox1.TabIndex = 13;
-			this.textBox1.Text = "DataGridDatos.DataSource = engine.ReadFileAsDT(\"infile.txt\")";
-			this.textBox1.WordWrap = false;
-			// 
-			// label4
-			// 
-			this.label4.BackColor = System.Drawing.Color.Transparent;
-			this.label4.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
-			this.label4.ForeColor = System.Drawing.Color.White;
-			this.label4.Location = new System.Drawing.Point(8, 56);
-			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(152, 16);
-			this.label4.TabIndex = 12;
-			this.label4.Text = "Code to Read the File";
-			// 
-			// DataGridDatos
-			// 
-			this.DataGridDatos.CaptionVisible = false;
-			this.DataGridDatos.DataMember = "";
-			this.DataGridDatos.GridLineColor = System.Drawing.Color.FromArgb(((System.Byte)(224)), ((System.Byte)(224)), ((System.Byte)(224)));
-			this.DataGridDatos.HeaderForeColor = System.Drawing.SystemColors.ControlText;
-			this.DataGridDatos.Location = new System.Drawing.Point(8, 120);
-			this.DataGridDatos.Name = "DataGridDatos";
-			this.DataGridDatos.ParentRowsVisible = false;
-			this.DataGridDatos.ReadOnly = true;
-			this.DataGridDatos.Size = new System.Drawing.Size(664, 184);
-			this.DataGridDatos.TabIndex = 14;
-			// 
-			// frmEasyToDataTable
-			// 
-			this.AutoScaleBaseSize = new System.Drawing.Size(5, 14);
-			this.ClientSize = new System.Drawing.Size(680, 496);
-			this.Controls.Add(this.DataGridDatos);
-			this.Controls.Add(this.label4);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.cmdRun);
-			this.Controls.Add(this.txtData);
-			this.Controls.Add(this.label3);
-			this.Controls.Add(this.textBox1);
-			this.Name = "frmEasyToDataTable";
-			this.Text = "FileHelpers - Read as DataTable";
-			this.Controls.SetChildIndex(this.pictureBox3, 0);
-			this.Controls.SetChildIndex(this.textBox1, 0);
-			this.Controls.SetChildIndex(this.label3, 0);
-			this.Controls.SetChildIndex(this.txtData, 0);
-			this.Controls.SetChildIndex(this.cmdRun, 0);
-			this.Controls.SetChildIndex(this.label1, 0);
-			this.Controls.SetChildIndex(this.label4, 0);
-			this.Controls.SetChildIndex(this.DataGridDatos, 0);
-			((System.ComponentModel.ISupportInitialize)(this.DataGridDatos)).EndInit();
-			this.ResumeLayout(false);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmEasyToDataTable));
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.cmdRun = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.DataGridDatos = new System.Windows.Forms.DataGrid();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(578, 7);
+            // 
+            // txtData
+            // 
+            this.txtData.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtData.Location = new System.Drawing.Point(8, 328);
+            this.txtData.Multiline = true;
+            this.txtData.Name = "txtData";
+            this.txtData.ReadOnly = true;
+            this.txtData.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtData.Size = new System.Drawing.Size(664, 136);
+            this.txtData.TabIndex = 1;
+            this.txtData.Text = resources.GetString("txtData.Text");
+            this.txtData.WordWrap = false;
+            // 
+            // cmdRun
+            // 
+            this.cmdRun.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(110)))));
+            this.cmdRun.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdRun.ForeColor = System.Drawing.Color.Gainsboro;
+            this.cmdRun.Location = new System.Drawing.Point(336, 8);
+            this.cmdRun.Name = "cmdRun";
+            this.cmdRun.Size = new System.Drawing.Size(152, 32);
+            this.cmdRun.TabIndex = 0;
+            this.cmdRun.Text = "RUN >>";
+            this.cmdRun.UseVisualStyleBackColor = false;
+            this.cmdRun.Click += new System.EventHandler(this.cmdRun_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(8, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Output DataTable";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(8, 312);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(264, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Input Data to the FileHelperEngine";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(8, 75);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(664, 39);
+            this.textBox1.TabIndex = 13;
+            this.textBox1.Text = "FileHelpersEngine engine = new FileHelpersEngine(typeof(CustomerFixed));\r\nDataGri" +
+                "dDatos.DataSource = engine.ReadFileAsDT(\"infile.txt\");";
+            this.textBox1.WordWrap = false;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(8, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(152, 16);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Code to Read the File";
+            // 
+            // DataGridDatos
+            // 
+            this.DataGridDatos.CaptionVisible = false;
+            this.DataGridDatos.DataMember = "";
+            this.DataGridDatos.GridLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.DataGridDatos.HeaderForeColor = System.Drawing.SystemColors.ControlText;
+            this.DataGridDatos.Location = new System.Drawing.Point(8, 134);
+            this.DataGridDatos.Name = "DataGridDatos";
+            this.DataGridDatos.ParentRowsVisible = false;
+            this.DataGridDatos.ReadOnly = true;
+            this.DataGridDatos.Size = new System.Drawing.Size(664, 170);
+            this.DataGridDatos.TabIndex = 14;
+            // 
+            // frmEasyToDataTable
+            // 
+            this.AutoScaleBaseSize = new System.Drawing.Size(5, 14);
+            this.ClientSize = new System.Drawing.Size(680, 496);
+            this.Controls.Add(this.DataGridDatos);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmdRun);
+            this.Controls.Add(this.txtData);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Name = "frmEasyToDataTable";
+            this.Text = "FileHelpers - Read as DataTable";
+            this.Controls.SetChildIndex(this.pictureBox3, 0);
+            this.Controls.SetChildIndex(this.textBox1, 0);
+            this.Controls.SetChildIndex(this.label3, 0);
+            this.Controls.SetChildIndex(this.txtData, 0);
+            this.Controls.SetChildIndex(this.cmdRun, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.label4, 0);
+            this.Controls.SetChildIndex(this.DataGridDatos, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridDatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
