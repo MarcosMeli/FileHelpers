6aa5e976f5f55afd395b0605d833f8de1693c1e9
Marcos Meli <marcos@filehelpers.com>
2007-04-12 18:58:40 +0000

    git-svn-id: svn://localhost/trunk@451 fafefce7-770d-0410-b8bf-a6f642d37c66

--- Libs/FileReplace/Main.cs
+++ Libs/FileReplace/Main.cs
@@ -66,7 +66,7 @@ namespace FileReplace
 			reader.Close();
 
 			StreamWriter writer = new StreamWriter(destFile, false, Encoding.Default);
-			writer.Write(originalStr.Replace(srcString, newString));
+			writer.Write(ReplaceEx(originalStr, srcString, newString));
 			writer.Close();
 
 			if (mVerbose) Console.WriteLine("Replaced: '" + FirstChars(srcString, 25) + "' --> '" + FirstChars(newString, 25) + "'.");
@@ -86,6 +86,31 @@ namespace FileReplace
 			Console.WriteLine("");
 			Console.WriteLine("   or");
 			Console.WriteLine("FileReplace.exe destFile srcString -f destSourceFile");
+		}
+
+		private static string ReplaceEx(string original, 
+			string pattern, string replacement)
+		{
+			int count, position0, position1;
+			count = position0 = position1 = 0;
+			string upperString = original.ToUpper();
+			string upperPattern = pattern.ToUpper();
+			int inc = (original.Length/pattern.Length) * 
+				(replacement.Length-pattern.Length);
+			char [] chars = new char[original.Length + Math.Max(0, inc)];
+			while( (position1 = upperString.IndexOf(upperPattern, 
+				position0)) != -1 )
+			{
+				for ( int i=position0 ; i < position1 ; ++i )
+					chars[count++] = original[i];
+				for ( int i=0 ; i < replacement.Length ; ++i )
+					chars[count++] = replacement[i];
+				position0 = position1+pattern.Length;
+			}
+			if ( position0 == 0 ) return original;
+			for ( int i=position0 ; i < original.Length ; ++i )
+				chars[count++] = original[i];
+			return new string(chars, 0, count);
 		}
 	}
 }
