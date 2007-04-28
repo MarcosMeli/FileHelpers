1bdc41d670b3c62b1cf724f35591be8fa7dd33f9
Xenolinguist <xenolinguist@gmail.com>
2007-04-28 06:05:15 +0000

    IronPython Demo tweaks FIX: Included the correct batch file (demo.bat) this time!  Moved ip.bat (IronPython interactive console helper) to the Libs folder.
    MOD: Commented and updated the demo python scripts.
    
    git-svn-id: svn://localhost/trunk@484 fafefce7-770d-0410-b8bf-a6f642d37c66

--- FileHelpers.Demos/Demo.IronPython.Hosting/test.py
+++ FileHelpers.Demos/Demo.IronPython.Hosting/test.py
@@ -1,4 +1,8 @@
 def main(engine, record) :
+    # We don't yet know which type of record object this is,
+    # but both possible types have a common field called "CopyrightDetailCode"
+    # which is a record type indicator.  If this field contains "C"
+    # then the record must be a CopyrightRecord - otherwise it's a DetailRecord.
     if record.CopyrightDetailCode == "C" :
         print "CINLIST file dated", record.FileVersionDate, "sequence", record.VolumeSequenceNumber
         print "=======================================\n"
