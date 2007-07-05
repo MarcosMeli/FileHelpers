91651f4eca3bc209e41da3cb05f96b1d35e0d8e5
Marcos Meli <marcos@filehelpers.com>
2007-07-05 17:11:03 +0000

    FIX: A problem in the PocketPc 2003 found and fixed by Christoph Roeper
    git-svn-id: svn://localhost/trunk@543 fafefce7-770d-0410-b8bf-a6f642d37c66

--- FileHelpers/Helpers/StreamHelper.cs
+++ FileHelpers/Helpers/StreamHelper.cs
@@ -39,31 +39,34 @@ namespace FileHelpers
 				{
 					fs = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.ReadWrite);
 
-					if (fs.Length >= 2)
-					{
-						fs.Seek(-2, SeekOrigin.End);
+                    if (fs.Length >= 2)
+                    {
+#if MINI
+                        fs.Seek(2, SeekOrigin.End);
+#else
+                        fs.Seek(-2, SeekOrigin.End);
+#endif
 
-						if (fs.ReadByte() == 13)
-						{
-							if (fs.ReadByte() == 10)
-							{
-								int nowRead;
-								do
-								{
-									fs.Seek(-2, SeekOrigin.Current);
-									nowRead = fs.ReadByte();
-								} while (nowRead == 13 || nowRead == 10);
-							}
-						}
-						else
-							fs.ReadByte();
+                        if (fs.ReadByte() == 13)
+                        {
+                            if (fs.ReadByte() == 10)
+                            {
+                                int nowRead;
+                                do
+                                {
+                                    fs.Seek(-2, SeekOrigin.Current);
+                                    nowRead = fs.ReadByte();
+                                } while (nowRead == 13 || nowRead == 10);
+                            }
+                        }
+                        else
+                            fs.ReadByte();
 
-						fs.WriteByte(13);
-						fs.WriteByte(10);
+                        fs.WriteByte(13);
+                        fs.WriteByte(10);
+                    }
 
-					}
-
-					res = new StreamWriter(fs, encode);
+				    res = new StreamWriter(fs, encode);
 
 				}
 				finally
