ecd066e22483629ec784fd7b2000d84b1b0b744e
Marcos Meli <marcos@filehelpers.com>
2007-08-01 14:11:31 +0000

    MOD: IronPython Demo small GUI changes ( add also (c) to Matt :P )
    git-svn-id: svn://localhost/trunk@599 fafefce7-770d-0410-b8bf-a6f642d37c66

--- FileHelpers.Demos/Demo.IronPython.Hosting/DemoForm.designer.cs
+++ FileHelpers.Demos/Demo.IronPython.Hosting/DemoForm.designer.cs
@@ -28,18 +28,25 @@ namespace Demo.IronPython.Hosting
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DemoForm));
             this.LoadModuleButton = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.LoadDataButton = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.ProcessButton = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // LoadModuleButton
             // 
             this.LoadModuleButton.Location = new System.Drawing.Point(14, 8);
             this.LoadModuleButton.Name = "LoadModuleButton";
-            this.LoadModuleButton.Size = new System.Drawing.Size(84, 34);
+            this.LoadModuleButton.Size = new System.Drawing.Size(113, 26);
             this.LoadModuleButton.TabIndex = 0;
             this.LoadModuleButton.Text = "Load Python Module";
             this.LoadModuleButton.UseVisualStyleBackColor = true;
@@ -47,25 +54,23 @@ namespace Demo.IronPython.Hosting
             // 
             // textBox1
             // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox1.BackColor = System.Drawing.Color.Maroon;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.textBox1.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.textBox1.Location = new System.Drawing.Point(0, 48);
+            this.textBox1.Location = new System.Drawing.Point(3, 17);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox1.Size = new System.Drawing.Size(462, 306);
+            this.textBox1.Size = new System.Drawing.Size(377, 354);
             this.textBox1.TabIndex = 1;
             // 
             // LoadDataButton
             // 
-            this.LoadDataButton.Location = new System.Drawing.Point(104, 8);
+            this.LoadDataButton.Location = new System.Drawing.Point(133, 8);
             this.LoadDataButton.Name = "LoadDataButton";
-            this.LoadDataButton.Size = new System.Drawing.Size(84, 34);
+            this.LoadDataButton.Size = new System.Drawing.Size(106, 26);
             this.LoadDataButton.TabIndex = 2;
             this.LoadDataButton.Text = "Load CINLIST file";
             this.LoadDataButton.UseVisualStyleBackColor = true;
@@ -74,25 +79,82 @@ namespace Demo.IronPython.Hosting
             // ProcessButton
             // 
             this.ProcessButton.Enabled = false;
-            this.ProcessButton.Location = new System.Drawing.Point(194, 8);
+            this.ProcessButton.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ProcessButton.Location = new System.Drawing.Point(245, 8);
             this.ProcessButton.Name = "ProcessButton";
-            this.ProcessButton.Size = new System.Drawing.Size(84, 34);
+            this.ProcessButton.Size = new System.Drawing.Size(105, 26);
             this.ProcessButton.TabIndex = 3;
             this.ProcessButton.Text = "Process!";
             this.ProcessButton.UseVisualStyleBackColor = true;
             this.ProcessButton.Click += new System.EventHandler(this.ProcessButton_Click);
             // 
-            // Form1
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.textBox2.Location = new System.Drawing.Point(3, 17);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(286, 354);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = resources.GetString("textBox2.Text");
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(0, 40);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(383, 374);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Output";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Location = new System.Drawing.Point(386, 40);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(292, 374);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Readme";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Gray;
+            this.label1.Location = new System.Drawing.Point(589, 1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "(c) Matt Campbell";
+            // 
+            // DemoForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(462, 354);
+            this.ClientSize = new System.Drawing.Size(678, 417);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.ProcessButton);
             this.Controls.Add(this.LoadDataButton);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.LoadModuleButton);
-            this.Name = "Form1";
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "DemoForm";
             this.Text = "IronPython Demo";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -105,6 +167,10 @@ namespace Demo.IronPython.Hosting
         private System.Windows.Forms.Button LoadDataButton;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Button ProcessButton;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
 
 
     }
