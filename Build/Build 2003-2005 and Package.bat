711056276e0349cd4d463df7568a5da3c1ce9309
Marcos Meli <marcos@filehelpers.com>
2009-05-21 20:16:57 +0000

    Good bye Visual Studio 2003 and .NET Framework 1.1 support
    git-svn-id: svn://localhost/trunk@667 fafefce7-770d-0410-b8bf-a6f642d37c66

--- Build/Build 2003-2005 and Package.bat
+++
@@ -1,5 +0,0 @@
-@ECHO ------------------------------------------
-@ECHO   Generating a Release of the FileHelpers
-@ECHO ------------------------------------------
-@..\libs\NAnt\NAnt.exe -nologo all
-@pause
--- Build/Build.2003.bat
+++
@@ -1,5 +0,0 @@
-@ECHO ---------------------------------
-@ECHO   Building FileHelpers (package)
-@ECHO ---------------------------------
-@..\libs\NAnt\NAnt.exe -nologo build2003
-@pause
