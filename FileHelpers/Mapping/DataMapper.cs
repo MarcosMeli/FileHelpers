23ca11e814f1799febedc2cc1cffd1b9a30ed3e9
Marcos Meli <marcos@filehelpers.com>
2009-05-26 03:37:09 +0000

    DataMapper non generics inherit from Generics
    git-svn-id: svn://localhost/trunk@686 fafefce7-770d-0410-b8bf-a6f642d37c66

--- FileHelpers/Mapping/DataMapper.cs
+++ FileHelpers/Mapping/DataMapper.cs
@@ -4,57 +4,55 @@
 
 #endregion
 
-#undef GENERICS
-//#define GENERICS
-//#if NET_2_0
+#define GENERICS
+
 
 using System;
 using System.Collections;
 using System.ComponentModel;
 using System.Data;
 using System.Reflection;
-#if GENERICS
 using System.Collections.Generic;
-#endif
 
 
 namespace FileHelpers.Mapping
 {
 
+    public sealed class DataMapper
+        :DataMapper<object>
+    {
+        public DataMapper(Type recordType)
+            :base(recordType)
+        {
+            
+        }
+    }
+
+
 	/// <summary>
     /// <para>A class to provide DataTable - DataReader - Records operations.</para>
     /// <para>(Use it at your own risk, API can change a lot in future version)</para>
 	/// </summary>
-#if ! GENERICS
-    public sealed class DataMapper
-#else
     /// <typeparam name="T">The record Type</typeparam>
-    public sealed class DataMapper<T>
-#endif
+    public class DataMapper<T>
     {
-		RecordInfo mRecordInfo;
+		internal RecordInfo mRecordInfo;
 		
-#if ! GENERICS
+
         /// <summary>
-        /// Create a new Mapping for the record Type 't'.
-        /// </summary>
-        /// <param name="recordType">The record class.</param>
-        public DataMapper(Type recordType)
+		/// Create a new Mapping for the record Type 't'.
+		/// </summary>
+		public DataMapper()
+            :this(typeof(T))
+		{
+			
+		}
+
+        internal DataMapper(Type recordType)
         {
             mRecordInfo = new RecordInfo(recordType);
         }
 
-#else
-        /// <summary>
-		/// Create a new Mapping for the record Type 't'.
-		/// </summary>
-		/// <param name="t">The record class.</param>
-        public DataMapper()
-		{
-			mRecordInfo = new RecordInfo(typeof(T));
-		}
-
-#endif
         private int mInitialColumnOffset = 0;
 
         /// <summary>
@@ -97,15 +95,9 @@ namespace FileHelpers.Mapping
 		/// </summary>
 		/// <param name="dt">The source Datatable</param>
 		/// <returns>The mapped records contained in the DataTable</returns>
-#if ! GENERICS
-		public object[] MapDataTable2Records(DataTable dt)
-        {
-            ArrayList arr = new ArrayList(dt.Rows.Count);
-#else
 		public T[] MapDataTable2Records(DataTable dt)
         {
 			List<T> arr = new List<T>(dt.Rows.Count);
-#endif
 
             mMappings.TrimToSize();
             foreach (DataRow row in dt.Rows)
@@ -113,11 +105,7 @@ namespace FileHelpers.Mapping
 				arr.Add(MapRow2Record(row));
 			}
 			
-#if ! GENERICS
-			return (object[]) arr.ToArray(mRecordInfo.mRecordType);
-#else
 			return arr.ToArray();
-#endif
         }
 
 
@@ -127,15 +115,9 @@ namespace FileHelpers.Mapping
 		/// <param name="dr">The source DataRow</param>
 		/// <returns>The mapped record containing the values of the DataRow</returns>
 		[EditorBrowsable(EditorBrowsableState.Advanced)]
-#if ! GENERICS
-		public object MapRow2Record(DataRow dr)
-        {
-            object record = mRecordInfo.CreateRecordObject();
-#else
 		public T MapRow2Record(DataRow dr)
         {
 			T record = (T) mRecordInfo.CreateRecordObject();
-#endif
 			
 			for(int i = 0; i < mMappings.Count; i++)
 			{
@@ -151,15 +133,9 @@ namespace FileHelpers.Mapping
 		/// <param name="dr">The already opened DataReader</param>
 		/// <returns>The mapped record containing the values of the DataReader</returns>
 		[EditorBrowsable(EditorBrowsableState.Advanced)]
-#if ! GENERICS
-		public object MapRow2Record(IDataReader dr)
-		{
-            object record = mRecordInfo.CreateRecordObject();
-#else
 		public T MapRow2Record(IDataReader dr)
 		{
             T record = (T) mRecordInfo.CreateRecordObject();
-#endif
             //TypedReference t = TypedReference.MakeTypedReference(record, new FieldInfo[]) null);
 				
 			for(int i = 0; i < mMappings.Count; i++)
@@ -176,15 +152,9 @@ namespace FileHelpers.Mapping
 		/// </summary>
 		/// <param name="dt">The source Datatable</param>
 		/// <returns>The mapped records contained in the DataTable</returns>
-#if ! GENERICS
-        public object[] AutoMapDataTable2RecordsByIndex(DataTable dt)
-        {
-            ArrayList arr = new ArrayList(dt.Rows.Count);
-#else
         public T[] AutoMapDataTable2RecordsByIndex(DataTable dt)
 		{
             List<T> arr = new List<T>(dt.Rows.Count);
-#endif
 
             FieldInfo[] fields =
 				mRecordInfo.mRecordType.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.GetField |
@@ -205,11 +175,7 @@ namespace FileHelpers.Mapping
 
 			foreach (DataRow row in dt.Rows)
 			{
-#if ! GENERICS
-				object record = mRecordInfo.CreateRecordObject();
-#else
 				T record = (T) mRecordInfo.CreateRecordObject();
-#endif
                 //TypedReference t = TypedReference.MakeTypedReference(record, new FieldInfo[]) null);
 				
 				for(int i = 0; i < mMappings.Count; i++)
@@ -220,11 +186,7 @@ namespace FileHelpers.Mapping
 				arr.Add(record);
 			}
 			
-#if ! GENERICS
-            return (object[])arr.ToArray(mRecordInfo.mRecordType);
-#else
             return arr.ToArray();
-#endif
         }
 
 	
@@ -234,15 +196,9 @@ namespace FileHelpers.Mapping
 		/// <param name="connection">A valid connection (Opened or not)</param>
 		/// <param name="selectSql">The Sql statement used to return the records.</param>
 		/// <returns>The mapped records contained in the DataTable</returns>
-#if ! GENERICS
-        public object[] MapDataReader2Records(IDbConnection connection, string selectSql)
-        {
-            object[] res;
-#else
         public T[] MapDataReader2Records(IDbConnection connection, string selectSql)
         {
             T[] res;
-#endif
             
             ExHelper.CheckNullParam(connection, "connection");
 			ExHelper.CheckNullOrEmpty(selectSql, "selectSql");
@@ -281,15 +237,9 @@ namespace FileHelpers.Mapping
 		/// </summary>
 		/// <param name="dr">The source DataReader</param>
 		/// <returns>The mapped records contained in the DataTable</returns>
-#if ! GENERICS
-		public object[] MapDataReader2Records(IDataReader dr)
-        {
-            ArrayList arr = new ArrayList();
-#else
 		public T[] MapDataReader2Records(IDataReader dr)
         {
             List<T> arr = new List<T>();
-#endif
             ExHelper.CheckNullParam(dr, "dr");
 
 			mMappings.TrimToSize();
@@ -301,11 +251,7 @@ namespace FileHelpers.Mapping
 					arr.Add(MapRow2Record(dr));
 				}
 			}
-#if ! GENERICS			
-            return (object[])arr.ToArray(mRecordInfo.mRecordType);
-#else
 			return arr.ToArray();
-#endif
         }
 
 
@@ -316,13 +262,8 @@ namespace FileHelpers.Mapping
 		/// </summary>
 		/// <param name="dr">The source DataReader</param>
 		/// <returns>The mapped records contained in the DataTable</returns>
-#if ! GENERICS
-		public IEnumerator MapDataReader2RecordsEnum(IDataReader dr)
-        {
-#else
 		public IEnumerator<T> MapDataReader2RecordsEnum(IDataReader dr)
         {
-#endif
             ExHelper.CheckNullParam(dr, "dr");
 
 			mMappings.TrimToSize();
@@ -435,4 +376,3 @@ namespace FileHelpers.Mapping
 
 
 }
-//#endif
