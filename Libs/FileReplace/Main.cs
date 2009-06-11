c747ad4bd032e162ef751f386c5d875a09044398
Marcos Meli <marcos@filehelpers.com>
2009-06-11 02:44:00 +0000

    git-svn-id: svn://localhost/trunk@689 fafefce7-770d-0410-b8bf-a6f642d37c66

--- Libs/FileReplace/Main.cs
+++ Libs/FileReplace/Main.cs
@@ -7,110 +7,128 @@ namespace FileReplace
 	/// <summary>
 	/// Summary description for Class1.
 	/// </summary>
-	class Replacer
-	{
-		/// <summary>
-		/// The main entry point for the application.
-		/// </summary>
-		[STAThread]
-		static void Main(string[] args)
-		{
-			if (args.Length < 3)
-				ShowSyntaxs();
-			else
-			{
-				string destFile, srcString, newString;
+    class Replacer
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length < 3)
+                ShowSyntaxs();
+            else
+            {
+                string destFile, srcString, newString;
 
-				destFile = args[0];
-				srcString = args[1];
-				newString = args[2];
+                destFile = args[0];
+                srcString = args[1];
+                newString = args[2];
+                int replaceMode = 0;
+                if (args.Length > 3)
+                    replaceMode = Convert.ToInt32(args[3]);
 
-				if (srcString == string.Empty)
-				{
-					ShowSyntaxs();
-					Environment.Exit(-1);
-					return;
-				}
 
-				if (newString == "-f")
-				{
-					if (args.Length < 4)
-					{
-						ShowSyntaxs();
-						Environment.Exit(-1);
-						return;
-					}
+                if (srcString == string.Empty)
+                {
+                    ShowSyntaxs();
+                    Environment.Exit(-1);
+                    return;
+                }
 
-					System.IO.StreamReader reader = new StreamReader(args[3]);
-					newString = reader.ReadToEnd();
-					reader.Close();
-				
-				}
+                if (newString == "-f")
+                {
+                    if (args.Length < 4)
+                    {
+                        ShowSyntaxs();
+                        Environment.Exit(-1);
+                        return;
+                    }
 
-				if (args[args.Length - 1].ToLower() == "-v")
-					mVerbose = false;
+                    System.IO.StreamReader reader = new StreamReader(args[3]);
+                    newString = reader.ReadToEnd();
+                    reader.Close();
 
-				foreach (string	file in Directory.GetFiles(Path.GetDirectoryName(destFile), Path.GetFileName(destFile)))
-				{
-					ReplaceFile(file, newString, srcString);
-				}
+                }
 
-			}
-		}
+                if (args[args.Length - 1].ToLower() == "-e")
+                    mEscape = true;
 
-		static bool mVerbose = true;
-		private static void ReplaceFile(string destFile, string newString, string srcString)
-		{
-			System.IO.StreamReader reader = new StreamReader(destFile, Encoding.Default);
-			string originalStr = reader.ReadToEnd();
-			reader.Close();
+                foreach (string file in Directory.GetFiles(Path.GetDirectoryName(destFile), Path.GetFileName(destFile)))
+                {
+                    ReplaceFile(file, newString, srcString);
+                }
 
-			StreamWriter writer = new StreamWriter(destFile, false, Encoding.Default);
-			writer.Write(ReplaceEx(originalStr, srcString, newString));
-			writer.Close();
+            }
+        }
+        static bool mEscape = false;
+        private static void ReplaceFile(string destFile, string newString, string srcString)
+        {
+            System.IO.StreamReader reader = new StreamReader(destFile, Encoding.Default);
+            string originalStr = reader.ReadToEnd();
+            reader.Close();
 
-			if (mVerbose) Console.WriteLine("Replaced: '" + FirstChars(srcString, 25) + "' --> '" + FirstChars(newString, 25) + "'.");
-		}
+            StreamWriter writer = new StreamWriter(destFile, false, Encoding.Default);
 
-		private static string FirstChars(string source, int chars)
-		{
-			return source.Substring(0, Math.Min(chars, source.Length)).Replace(Environment.NewLine, " ");
-			
-		}
+            if (mEscape)
+            {
+                srcString = string.Format(srcString);
+                newString = string.Format(newString);
+            }
 
-		private static void ShowSyntaxs()
-		{
-			Console.WriteLine("Syntaxis:");
-			Console.WriteLine("");
-			Console.WriteLine("FileReplace.exe destFile srcString destString");
-			Console.WriteLine("");
-			Console.WriteLine("   or");
-			Console.WriteLine("FileReplace.exe destFile srcString -f destSourceFile");
-		}
+            writer.Write(ReplaceIgnoringCase(originalStr, srcString, newString));
+            writer.Close();
 
-		private static string ReplaceEx(string original, 
-			string pattern, string replacement)
-		{
-			int count, position0, position1;
-			count = position0 = position1 = 0;
-			string upperString = original.ToUpper();
-			string upperPattern = pattern.ToUpper();
-			int inc = (original.Length/pattern.Length) * 
-				(replacement.Length-pattern.Length);
-			char [] chars = new char[original.Length + Math.Max(0, inc)];
-			while( (position1 = upperString.IndexOf(upperPattern, 
-				position0)) != -1 )
-			{
-				for ( int i=position0 ; i < position1 ; ++i )
-					chars[count++] = original[i];
-				for ( int i=0 ; i < replacement.Length ; ++i )
-					chars[count++] = replacement[i];
-				position0 = position1+pattern.Length;
-			}
-			if ( position0 == 0 ) return original;
-			for ( int i=position0 ; i < original.Length ; ++i )
-				chars[count++] = original[i];
-			return new string(chars, 0, count);
-		}
-	}
+            Console.WriteLine("Replaced: '" + FirstChars(srcString, 25) + "' --> '" + FirstChars(newString, 25) + "'.");
+        }
+
+        private static string FirstChars(string source, int chars)
+        {
+            return source.Substring(0, Math.Min(chars, source.Length)).Replace(Environment.NewLine, " ");
+
+        }
+
+        private static void ShowSyntaxs()
+        {
+            Console.WriteLine("Syntaxis:");
+            Console.WriteLine("");
+            Console.WriteLine("FileReplace.exe destFile srcString destString");
+            Console.WriteLine("");
+            Console.WriteLine("   or");
+            Console.WriteLine("FileReplace.exe destFile srcString -f destSourceFile");
+            Console.WriteLine("   with escaped string");
+            Console.WriteLine("FileReplace.exe destFile srcString destString -e");
+        }
+
+        public static string ReplaceIgnoringCase(string original, string oldValue, string newValue)
+        {
+            return Replace(original, oldValue, newValue, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public static string Replace(string original, string oldValue, string newValue, StringComparison comparisionType)
+        {
+            string result = original;
+
+            if (!string.IsNullOrEmpty(newValue))
+            {
+                int index = -1;
+                int lastIndex = 0;
+
+                StringBuilder buffer = new StringBuilder(original.Length);
+
+                while ((index = original.IndexOf(oldValue, index + 1, comparisionType)) >= 0)
+                {
+                    buffer.Append(original, lastIndex, index - lastIndex);
+                    buffer.Append(newValue);
+
+                    lastIndex = index + oldValue.Length;
+                }
+                buffer.Append(original, lastIndex, original.Length - lastIndex);
+
+                result = buffer.ToString();
+            }
+
+            return result;
+        }
+    }
 }
