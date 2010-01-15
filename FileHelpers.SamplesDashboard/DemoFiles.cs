dd3399c96545440447aa7ff8a4e9fb7514f2e579
Marcos Meli <marcos@filehelpers.com>
2010-01-15 12:19:42 +0000

    I forgot this file :)
    git-svn-id: svn://localhost/trunk@771 fafefce7-770d-0410-b8bf-a6f642d37c66

---
+++ FileHelpers.SamplesDashboard/DemoFiles.cs
@@ -0,0 +1,9 @@
+﻿namespace FileHelpers
+{
+    public class DemoFiles
+    {
+        public string FileName { get; set; }
+        public string Contents { get; set; }
+        public NetLanguage Language { get; set; }
+    }
+}
