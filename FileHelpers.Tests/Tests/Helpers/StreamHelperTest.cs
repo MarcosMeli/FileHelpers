08c7fe16716a79af13373df6607b79a5bd81a19a
oowaratah <oowaratah@gmail.com>
2011-01-20 12:24:25 +0000

    Add streamhelper test
    git-svn-id: svn://localhost/trunk@862 fafefce7-770d-0410-b8bf-a6f642d37c66

---
+++ FileHelpers.Tests/Tests/Helpers/StreamHelperTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+using NUnit.Framework;
+using FileHelpers;
+
+namespace FileHelpers.Tests.Helpers
+{
+    /// <summary>
+    /// FileHelpers Helpers StreamHelper tests
+    /// Stream Helper opens files and optionally trims off blank lines at the end.
+    /// We access this through the engine with append lines to send an append command
+    /// </summary>
+    [TestFixture]
+    class StreamHelperTest
+    {
+        [Test(Description = "A basic Windows windows CR/LF file")]
+        public void TestDosFile()
+        {
+            SimpleData testdata = new SimpleData()
+            {
+
+                Record = "one record only"
+            };
+
+            var engine = new DelimitedFileEngine<SimpleData>();
+            using (var filename = new TempFileFactory())
+            {
+                String twoRecords = testdata.Record + StringHelper.NewLine + testdata.Record + StringHelper.NewLine;
+
+                ProcessAppend(testdata, engine, filename, twoRecords, "\r\n\r\n", "Dos");
+                ProcessAppend(testdata, engine, filename, twoRecords, "\n\n\n", "Unix");
+                ProcessAppend(testdata, engine, filename, twoRecords, "\r\r\r", "Macintosh");
+
+                GC.Collect();  //  Clean up the loose file stream from the testing
+            }
+        }
+
+        private static void ProcessAppend(SimpleData testdata, DelimitedFileEngine<SimpleData> engine, String filename, String twoRecords,
+            string LineEnds, string testname)
+        {
+                using (var fs = new StreamWriter(filename))
+                {
+                    fs.Write(testdata.Record);
+                    fs.Write(LineEnds);  // lots of blanks lines to trim
+                    fs.Close();
+                }
+
+                engine.AppendToFile(filename, testdata);
+
+                using (var input = new StreamReader(filename))
+                {
+                    String result = input.ReadToEnd();
+                    result.AssertEqualTo<String>(twoRecords, testname + ": Expected two records only on output");
+                    input.Close();
+                }
+        }
+
+
+        [DelimitedRecord(";")]
+        class SimpleData
+        {
+            internal String Record;
+        }
+    }
+}
