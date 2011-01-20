2a9316625bcb2685dbc8d1fdd94dbb85c6bb9c4f
oowaratah <oowaratah@gmail.com>
2011-01-20 12:27:24 +0000

    Add generic test writer
    git-svn-id: svn://localhost/trunk@864 fafefce7-770d-0410-b8bf-a6f642d37c66

--- FileHelpers.Tests/Tests/Common.cs
+++ FileHelpers.Tests/Tests/Common.cs
@@ -1,16 +1,29 @@
+using System;
 using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
 using FileHelpers;
 using FileHelpers.MasterDetail;
-using System.Collections;
-using System.IO;
 using MasterDetails = FileHelpers.MasterDetail.MasterDetails<object, object>;
 
 namespace FileHelpers.Tests
 {
-	// this class only adds the relative path to the saple files.
+
+    /// <summary>
+    /// This class only adds the relative path to the sample files. 
+    /// </summary>
 	public static class TestCommon
 	{
 	    //private static string mAssemblyLocation = string.Empty;
+        /// <summary>
+        /// Create a path to a directory or file given a list of directories
+        /// to get there.  Goes from project directory
+        /// </summary>
+        /// <remarks>.. takes it back from bin, Data is forward to data area</remarks>
+        /// <param name="pathElements">list of directories to navigate with optional filename</param>
+        /// <returns>Path to Data area</returns>
         public static string GetPath(params string[] pathElements)
         {
             var result = Path.GetFullPath(Path.Combine("..", "Data"));
@@ -23,22 +36,45 @@ namespace FileHelpers.Tests
             return result;
         }
 
+        /// <summary>
+        /// Create a temporary filename for work
+        /// </summary>
+        /// <param name="fileName">Filename for test</param>
+        /// <returns>Path to a working file</returns>
         public static string GetTempFile(string fileName)
         {
             return Path.Combine(Path.GetTempPath(), fileName);
         }
 
+        /// <summary>
+        /// Use an engine to read an array of objects
+        /// </summary>
+        /// <param name="engine">Engine to read file</param>
+        /// <param name="pathElements">List of directories and a filename in Data area</param>
+        /// <returns>objects from file</returns>
+        [Obsolete("Use ReadTest<T> instead" )]
         public static object[] ReadTest(FileHelperEngine engine, params string[] pathElements)
         {
             return engine.ReadFile(GetPath(pathElements));
         }
 
+        /// <summary>
+        /// Use an engine to read an array of objects
+        /// </summary>
+        /// <param name="engine">Engine to read file</param>
+        /// <param name="pathElements">List of directories and a filename in Data area</param>
+        /// <returns>objects from file</returns>
         public static T[] ReadTest<T>(FileHelperEngine<T> engine, params string[] pathElements) where T : class
         {
             return engine.ReadFile(GetPath(pathElements));
         }
 
-
+        /// <summary>
+        /// Read the file using the async engine
+        /// </summary>
+        /// <param name="engine">Engine to read the data</param>
+        /// <param name="pathElements">List of directories and a filename in Data area</param>
+        /// <returns>objects from file</returns>
         public static object[] ReadAllAsync(FileHelperAsyncEngine engine, params string[] pathElements)
         {
             ArrayList arr = new ArrayList();
@@ -53,10 +89,44 @@ namespace FileHelpers.Tests
 
         }
 
+        /// <summary>
+        /// Read the file using the async engine
+        /// </summary>
+        /// <param name="engine">Engine to read the data</param>
+        /// <param name="pathElements">List of directories and a filename in Data area</param>
+        /// <returns>objects from file</returns>
+        public static List<T> ReadAllAsync<T>(FileHelperAsyncEngine<T> engine, params string[] pathElements) where T : class
+        {
+            List<T> arr = new List<T>();
 
+            using (engine.BeginReadFile(GetPath(pathElements)))
+            {
+                while (engine.ReadNext() != null)
+                    arr.Add(engine.LastRecord);
+            }
+
+            return arr;
+
+        }
+
+        /// <summary>
+        /// Use an engine to read the first part of the file
+        /// </summary>
+        /// <param name="engine">Engine to read the data</param>
+        /// <param name="pathElements">List of directories and a filename in Data area</param>
         public static void BeginReadTest(FileHelperAsyncEngine engine, params string[] pathElements)
         {
             engine.BeginReadFile(GetPath(pathElements));
         }
-	}
+
+        /// <summary>
+        /// Use an engine to read the first part of the file
+        /// </summary>
+        /// <param name="engine">Engine to read the data</param>
+        /// <param name="pathElements">List of directories and a filename in Data area</param>
+        public static void BeginReadTest<T>(FileHelperAsyncEngine<T> engine, params string[] pathElements) where T : class
+        {
+            engine.BeginReadFile(GetPath(pathElements));
+        }
+    }
 }
