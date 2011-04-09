df304eba8e717873f3eb09db6a86d613236dccb6
Marcos Meli <marcos@filehelpers.com>
2011-04-09 03:27:39 +0000

    Adding IdeasBag (a list of pending changes for future reference) Relinking Diagrams
    git-svn-id: svn://localhost/trunk@912 fafefce7-770d-0410-b8bf-a6f642d37c66

---
+++ IdeasBag/StreamInfoProvider.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+
+namespace FileHelpers
+{
+    internal sealed class StreamInfoProvider
+    {
+        private delegate long GetValue();
+
+
+        //private readonly bool mHasInfo;
+        //private readonly Stream mStream;
+        private readonly GetValue mLengthCalculator = () => -1;
+        private readonly GetValue mPositionCalculator = () => -1;
+
+        public StreamInfoProvider(TextReader reader)
+        {
+            if (reader is StreamReader)
+            {
+                var stream = ((StreamReader)reader).BaseStream;
+                mLengthCalculator = () => stream.Length;
+                mPositionCalculator = () => stream.Position;
+            }
+            else if (reader is InternalStringReader)
+            {
+                var stream = (InternalStringReader)reader;
+                mLengthCalculator = () => stream.Length;
+                mPositionCalculator = () => stream.Position;
+            }
+        }
+
+        public StreamInfoProvider(TextWriter writer)
+        {
+            if (writer is StreamWriter)
+            {
+                var stream = ((StreamWriter)writer).BaseStream;
+                mLengthCalculator = () => stream.Length;
+                mPositionCalculator = () => stream.Position;
+            }
+
+        }
+
+        public long Position
+        {
+            get { return mPositionCalculator(); }
+        }
+
+        public long TotalBytes
+        {
+            get { return mLengthCalculator(); }
+        }
+    }
+}
