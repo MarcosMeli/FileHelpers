94645d6a4669c8aedb269fea31038c20c2194a96
Marcos Meli <marcos@filehelpers.com>
2012-07-20 12:08:22 +0000

    Build decomposition
    git-svn-id: svn://localhost/trunk@950 fafefce7-770d-0410-b8bf-a6f642d37c66

--- Build/VersionInfo.cs
+++ Build/VersionInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 [assembly : AssemblyFileVersion("-CustomVersion-")]
 
 [assembly : AssemblyDescription("A free and easy to use .NET library to read/write data from fixed length or delimited records in files, strings or streams")]
-[assembly : AssemblyProduct("FileHelpers   http://www.filehelpers.com")]
+[assembly : AssemblyProduct("FileHelpers  http://www.filehelpers.com")]
 
 [assembly : AssemblyCompany("Devoo")]
 [assembly : AssemblyTrademark("FileHelpers")]
-[assembly : AssemblyCopyright("Copyright 2005-2011. Marcos Meli")]
+[assembly : AssemblyCopyright("Copyright 2005-2012. Marcos Meli")]
 [assembly : AssemblyCulture("")]
 [assembly : AssemblyConfiguration("")]
 [assembly : ComVisible(false)]
