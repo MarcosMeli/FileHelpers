0cc82f8582a1a364463a97df1cf6feaf2aee1ef1
Marcos Meli <marcos@filehelpers.com>
2012-07-20 18:19:12 +0000

    Version Info
    git-svn-id: svn://localhost/trunk@967 fafefce7-770d-0410-b8bf-a6f642d37c66

--- FileHelpers/VersionInfo.cs
+++ FileHelpers/VersionInfo.cs
@@ -1,19 +1,16 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
 // The version of the Library
-[assembly : AssemblyVersion("2.9.10.0")]
-[assembly : AssemblyFileVersion("2.9.10.0")]
+[assembly : AssemblyVersion("2.9.15.0")]
+[assembly : AssemblyFileVersion("2.9.15.0")]
 
 [assembly : AssemblyDescription("A free and easy to use .NET library to read/write data from fixed length or delimited records in files, strings or streams")]
-[assembly : AssemblyProduct("FileHelpers   http://www.filehelpers.com")]
+[assembly : AssemblyProduct("FileHelpers  http://www.filehelpers.com")]
 
 [assembly : AssemblyCompany("Devoo")]
 [assembly : AssemblyTrademark("FileHelpers")]
-[assembly : AssemblyCopyright("Copyright 2005-2011. Marcos Meli")]
+[assembly : AssemblyCopyright("Copyright 2005-2012. Marcos Meli")]
 [assembly : AssemblyCulture("")]
 [assembly : AssemblyConfiguration("")]
 [assembly : ComVisible(false)]
