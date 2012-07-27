2ebccefdba666f8ae89a954475b9fd1791772c62
Marcos Meli <marcos@filehelpers.com>
2012-07-27 16:31:51 +0000

    Auto ILMerge ExcelNPOIStorega and the old ExcelStorage
    git-svn-id: svn://localhost/trunk@973 fafefce7-770d-0410-b8bf-a6f642d37c66

--- References/NPOI/NPOI.Merged.bat
+++ References/NPOI/NPOI.Merged.bat
@@ -1,2 +1,2 @@
 REM Merged using ILMerge with Ionic.Zip.Reduced.dll
-ilmerge /internalize /ndebug /log:log.txt /t:library /out:NPOI.Merged.dll NPOI.dll Ionic.Zip.dll
+..\..\Libs\ILMerge\ilmerge /internalize /ndebug /log:log.txt /t:library /out:NPOI.Merged.dll NPOI.dll Ionic.Zip.dll
