b4fb11c6412f5d767803d8ac2831b5ffb3c85cd3
Marcos Meli <marcos@filehelpers.com>
2012-08-16 23:13:53 -0300

    BenchMark refactors

--- FileHelpers.Benchmarks/BenchMark.cs
+++ FileHelpers.Benchmarks/BenchMark.cs
@@ -15,12 +15,13 @@ namespace FileHelpers.Benchmarks
         /// <summary>
         /// Performs a series of tests to show time differences
         /// </summary>
-        /// <param name="args">Command line arguments</param>
 		[STAThread]
-		static void Main(string[] args)
+		static void Main()
 		{
 		    string file = Path.GetTempFileName();
             var records = 1000000;
+
+            Console.WriteLine("Generating file with " + records +" records");
             CreateSampleFixedString(file, records);
             var engine = new FileHelperAsyncEngine<FixedSampleRecord>();
             //TestFixedLengthRecord(engine, file);
@@ -31,24 +32,23 @@ namespace FileHelpers.Benchmarks
             //engine.AfterWriteRecord += new FileHelpers.Events.AfterWriteHandler<FixedSampleRecord>(engine_AfterWriteRecord);
             //engine.BeforeWriteRecord += new FileHelpers.Events.BeforeWriteHandler<FixedSampleRecord>(engine_BeforeWriteRecord);
 
-            if (args.Length > 0)
-            {
-                Console.WriteLine("Press enter to start testing");
-                Console.ReadLine();
-            }
             // Read one time to load it on memoty
-		    TestFixedLengthRecord(engine, file);
 
-            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("File generated, precaching...");
 
+            TestFixedLengthRecord(engine, file);
+
+            System.Threading.Thread.Sleep(2000);
+
+            Console.WriteLine("Start Test...");
             long start = DateTime.Now.Ticks;
             TestFixedLengthRecord(engine, file);
 
             var ts = new TimeSpan(DateTime.Now.Ticks - start);
 
-            Console.WriteLine("Total Time: " + Math.Round(ts.TotalSeconds, 2));
             Console.WriteLine("Records: " + records);
-            Console.WriteLine(Math.Round(ts.TotalMilliseconds / records, 2) + " ms per record");
+            Console.WriteLine("Total Time: " + Math.Round(ts.TotalSeconds, 3) + " seconds");
+            Console.WriteLine(Math.Round(ts.TotalMilliseconds / records, 3) + " ms per record");
             
             Console.ReadKey();
 		    
