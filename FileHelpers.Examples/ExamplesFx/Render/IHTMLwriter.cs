222448f08f1f70f79a8996a92579c15c6b4c298b
Marcos Meli <marcos@filehelpers.com>
2013-07-02 17:16:05 -0300

    Refactor files

---
+++ FileHelpers.Examples/ExamplesFx/Render/IHTMLwriter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExamplesFx
+{
+    /// <summary>
+    /// Enable object to output itself as HTML code
+    /// </summary>
+    public interface IHtmlWriter
+    {
+        /// <summary>
+        /// Generate the HTML for the index details to the
+        /// string builder and optionally write any
+        /// other html output files
+        /// </summary>
+        /// <param name="index">html version of the index</param>
+        void OutputHtml(StringBuilder index, int indent);
+    }
+}
