bceea57cb2818012e593451bdc6ef362fa1aeab3
Matthias Cavigelli <cavigelli@gmail.com>
2014-04-15 21:13:53 +0200

    Added unit test to get exception with tiny typo
    - asserting whole message seemed to be to much duplication to the implementation

---
+++ FileHelpers.Tests/Tests/FileHelpers/DelimitedFieldTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+using Moq;
+using NUnit.Framework;
+
+namespace FileHelpers.Tests.FileHelpers
+{
+    [TestFixture]
+    public class DelimitedFieldTests
+    {
+        [Test]
+        public void GivenModeIsAlwaysQuotedWhenLineInfoIsNotQuotedThenAnExceptionIsThrown() {
+            var fi = new Mock<FieldInfo>();
+            fi.Setup(f => f.FieldType).Returns(typeof (string));
+            var fieldUnderTest = new DelimitedField(fi.Object, ";") {
+                QuoteChar = '%',
+                QuoteMode = QuoteMode.AlwaysQuoted
+            };
+            var invalidLineInfo = new LineInfo("Hello, World") {
+                mReader = new ForwardReader(new Mock<IRecordReader>().Object, 0),
+            };
+
+            Assert.Throws<BadUsageException>(() => fieldUnderTest.ExtractFieldString(invalidLineInfo));
+        } 
+    }
+}
