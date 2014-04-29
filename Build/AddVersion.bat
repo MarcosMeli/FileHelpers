61bafbfbd266467f3888027dc37ce650410b2bcb
Marcos <marcos@filehelpers.com>
2014-04-29 16:12:39 -0300

    Version Adder

--- Build/AddVersion.bat
+++ Build/AddVersion.bat
@@ -1,2 +1,3 @@
-Build\VersionAdder Build\CurrentVersion.msbuild "</CurrentVersion>" 
+VersionAdder CurrentVersion.msbuild "</CurrentVersion>"
+cd ..
 @%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild Build\FileHelpers.msbuild /t:version /tv:4.0 /nologo
