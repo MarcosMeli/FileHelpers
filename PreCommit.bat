3f0d78f338d58c125727a37fbdf415d094ee34ca
Marcos <marcos@filehelpers.com>
2014-04-29 16:09:36 -0300

    PreCommit Hook

--- PreCommit.bat
+++ PreCommit.bat
@@ -1,3 +1,2 @@
-VersionAdder CurrentVersion.msbuild "</CurrentVersion>" 
-@cd ..
+Build\VersionAdder Build\CurrentVersion.msbuild "</CurrentVersion>" 
 @%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild Build\FileHelpers.msbuild /t:version /tv:4.0 /nologo
