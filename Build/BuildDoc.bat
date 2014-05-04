f663926e3bda924d195dc4921cd687e03bbda137
Marcos Meli <marcos@filehelpers.com>
2014-05-04 02:42:20 -0300

    Remove custom Sandcastle using installed one

--- Build/BuildDoc.bat
+++ Build/BuildDoc.bat
@@ -2,4 +2,4 @@
 cd ..
 .nuget\NuGet.exe install .nuget\packages.config -OutputDirectory packages
 .nuget\NuGet.exe restore FileHelpers.sln
-powershell.exe -NoProfile -ExecutionPolicy unrestricted -Command "& {Import-Module '.\packages\psake.*\tools\psake.psm1'; invoke-psake .\Build\default.ps1 doc; if ($LastExitCode -ne 0) {write-host "ERROR: $LastExitCode" -fore RED; exit $lastexitcode} }"
+powershell.exe -NoProfile -ExecutionPolicy unrestricted -Command "& {Import-Module '.\packages\psake.*\tools\psake.psm1'; invoke-psake .\Build\default.ps1 docs; if ($LastExitCode -ne 0) {write-host "ERROR: $LastExitCode" -fore RED; exit $lastexitcode} }"
