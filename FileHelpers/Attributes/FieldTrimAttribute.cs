02bf72bca991bf9784b42e683ba1ac73e0e3a666
Marcos Meli <marcos@filehelpers.com>
2015-07-12 20:24:15 -0300

    Don't run Dynamic Tests

--- FileHelpers/Attributes/FieldTrimAttribute.cs
+++ FileHelpers/Attributes/FieldTrimAttribute.cs
@@ -14,20 +14,13 @@ namespace FileHelpers
 
         /// <summary>The TrimMode used after read.</summary>
         public TrimMode TrimMode { get; private set; }
-
-
-        private static readonly char[] WhitespaceChars = new char[] {
-            '\t', '\n', '\v', '\f', '\r', ' ', '\x00a0', '\u2000', '\u2001', '\u2002', '\u2003', '\u2004', '\u2005',
-            '\u2006', '\u2007', '\u2008',
-            '\u2009', '\u200a', '\u200b', '\u3000', '\ufeff'
-        };
-
+        
         #region "  Constructors  "
 
         /// <summary>Indicates the <see cref="TrimMode"/> used after read to truncate the field. By default trims the blank spaces and tabs.</summary>
         /// <param name="mode">The <see cref="TrimMode"/> used after read.</param>
         public FieldTrimAttribute(TrimMode mode)
-            : this(mode, WhitespaceChars) {}
+            : this(mode, LineInfo.WhitespaceChars) {}
 
         /// <summary>Indicates the <see cref="TrimMode"/> used after read to truncate the field. </summary>
         /// <param name="mode">The <see cref="TrimMode"/> used after read.</param>
