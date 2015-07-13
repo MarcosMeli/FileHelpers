b0595d6e65bd972c9ced2fc6a4865c94af8801b8
Marcos Meli <marcos@filehelpers.com>
2015-07-13 11:19:06 -0300

    Hide Loader

--- js/main.js
+++ js/main.js
@@ -2,17 +2,13 @@
 var _this = this;
 $(function () {
     var window_width = $(window).width();
-    /*Preloader*/
     $(window).load(function () { return setTimeout(function () { return $('body').addClass('loaded'); }, 200); });
     $('.show-search').click(function () {
         $('.search-out').fadeToggle("50", "linear");
         $('.search-out-text').focus();
     });
-    // Check first if any of the task is checked
     $('#task-card input:checkbox').each(function () { return checkbox_check(_this); });
-    // Task check box
     $('#task-card input:checkbox').change(function () { return checkbox_check(_this); });
-    // Check Uncheck function
     function checkbox_check(el) {
         if (!$(el).is(':checked')) {
             $(el).next().css('text-decoration', 'none');
