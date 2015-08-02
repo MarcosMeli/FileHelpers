acb59d2b169dd93c3db328fa7c3acdc0327004db
Marcos Meli <marcos@filehelpers.com>
2015-08-02 19:01:28 -0300

    More Examples

--- FileHelpers.Examples/Examples/10.QuickStart/10.ReadFileDelimited.cs
+++ FileHelpers.Examples/Examples/10.QuickStart/10.ReadFileDelimited.cs
@@ -3,8 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using FileHelpers;
 
-// Done
-
 namespace ExamplesFx
 {
     //-> Name: Read Delimited File
--- FileHelpers.Examples/Examples/10.QuickStart/40.WriteFileFixed.cs
+++ FileHelpers.Examples/Examples/10.QuickStart/40.WriteFileFixed.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using FileHelpers;
 
-// Done
 
 namespace ExamplesFx
 {
