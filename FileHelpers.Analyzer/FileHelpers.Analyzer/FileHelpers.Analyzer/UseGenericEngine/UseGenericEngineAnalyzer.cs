74a8a43d87351b6c4e127b1d8ae36efa402cd829
Marcos Meli <marcos@filehelpers.com>
2015-08-02 14:50:49 -0300

    Analyzer

--- FileHelpers.Analyzer/FileHelpers.Analyzer/FileHelpers.Analyzer/UseGenericEngine/UseGenericEngineAnalyzer.cs
+++ FileHelpers.Analyzer/FileHelpers.Analyzer/FileHelpers.Analyzer/UseGenericEngine/UseGenericEngineAnalyzer.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using Microsoft.CodeAnalysis;
@@ -15,12 +16,11 @@ namespace FileHelpersAnalyzer
     {
         public const string DiagnosticId = "UseGenericEngine";
 
-        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
-        private static readonly LocalizableString Title = "FileHelpers: You can use the generic engine";
-        private static readonly LocalizableString MessageFormat = "FileHelpers: You can use the generic engine";
-        private static readonly LocalizableString Description = "Is recommended to use the generic version of the engine";
+        private static readonly string Title = "FileHelpers: You can use the generic engine";
+        private static readonly string MessageFormat = "FileHelpers: You can use the generic engine";
+        private static readonly string Description = "Is recommended to use the generic version of the engine";
         
-            private const string Category = "Naming";
+        private const string Category = "Usage";
 
         private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
 
