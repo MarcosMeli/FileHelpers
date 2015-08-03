0ec297a86bec5e2c1be9baa0b4d6b0ff3ecf4b6e
Marcos Meli <marcos@filehelpers.com>
2015-08-03 16:31:27 -0300

    FileHelpers Analyzer

--- FileHelpers.Analyzer/FileHelpers.Analyzer/FileHelpers.Analyzer/Properties/AssemblyInfo.cs
+++ FileHelpers.Analyzer/FileHelpers.Analyzer/FileHelpers.Analyzer/Properties/AssemblyInfo.cs
@@ -27,6 +27,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.2.0")]
-[assembly: AssemblyFileVersion("0.2.0")]
-[assembly: AssemblyInformationalVersion("0.2.0")]
+[assembly: AssemblyVersion("0.2.1")]
+[assembly: AssemblyFileVersion("0.2.1")]
+[assembly: AssemblyInformationalVersion("0.2.1")]
