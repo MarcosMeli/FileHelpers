f923cda65ea3d6ee5e34349d3dc00509dd6f2aee
Marcos Meli <marcos@filehelpers.com>
2015-08-04 14:46:17 -0300

    Version Info

--- FileHelpers.Analyzer/FileHelpers.Analyzer/FileHelpers.Analyzer/Properties/AssemblyInfo.cs
+++ FileHelpers.Analyzer/FileHelpers.Analyzer/FileHelpers.Analyzer/Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using FileHelpersAnalyzer;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -9,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Devoo")]
 [assembly: AssemblyProduct("FileHelpersAnalyzer")]
-[assembly: AssemblyCopyright("Copyright ©2015 Devoo")]
+[assembly: AssemblyCopyright("Copyright © 2015 Devoo")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -27,6 +28,9 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+
+
+[assembly: AssemblyVersion(AnalyzerConfig.CurrentVersion)]
+[assembly: AssemblyFileVersion(AnalyzerConfig.CurrentVersion)]
+[assembly: AssemblyInformationalVersion(AnalyzerConfig.CurrentVersion)]
+
