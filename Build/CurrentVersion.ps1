c3fd92730448fd9c741d69af146c0e686df418d3
Marcos Meli <marcos@filehelpers.com>
2015-08-11 09:51:46 -0300

    Build only .net 4.0 and forward

--- Build/CurrentVersion.ps1
+++ Build/CurrentVersion.ps1
@@ -1,6 +1,6 @@
 properties {
     $AssemblyVersion = "3.2" 
-    $CurrentVersion = $AssemblyVersion + ".0" 
+    $CurrentVersion = $AssemblyVersion + ".1" 
     $FullCurrentVersion = $CurrentVersion + ".0"
     $VisibleVersion = $CurrentVersion + ""
 }
--- FileHelpers/VersionInfo.cs
+++ FileHelpers/VersionInfo.cs
@@ -3,8 +3,8 @@ using System.Runtime.InteropServices;
 
 // The version of the Library
 [assembly : AssemblyVersion("3.2.0")]
-[assembly : AssemblyFileVersion("3.2.0.0")]
-[assembly : AssemblyInformationalVersion("3.2.0")]
+[assembly : AssemblyFileVersion("3.2.1.0")]
+[assembly : AssemblyInformationalVersion("3.2.1")]
 
 [assembly : AssemblyDescription("A free and easy to use .NET library to read/write data from fixed length or delimited records in files, strings or streams")]
 [assembly : AssemblyProduct("FileHelpers  http://www.filehelpers.net")]
