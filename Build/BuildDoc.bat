bf85938e65a426103eb6e05c50860d8cfaa6135f
Marcos Meli <marcos@filehelpers.com>
2015-08-14 00:38:13 -0300

    Docs Generation

--- Build/BuildDoc.bat
+++ Build/BuildDoc.bat
@@ -5,3 +5,5 @@ SET EnableNuGetPackageRestore=true
 .nuget\NuGet.exe restore ..\FileHelpers.sln
 cd ..
 powershell.exe -NoProfile -ExecutionPolicy unrestricted -Command "& {Import-Module '.\packages\psake.*\tools\psake.psm1'; invoke-psake .\Build\default.ps1 docs; if ($LastExitCode -ne 0) {write-host "ERROR: $LastExitCode" -fore RED; exit $lastexitcode} }"
+cd Build
+adddocs.bat
