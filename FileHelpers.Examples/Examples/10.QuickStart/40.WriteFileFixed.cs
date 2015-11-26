7fc67e657fe8cae84d4e45fe82d2d9b5c6d3324d
Stephen Moon <stephen@logicalmoon.com>
2015-11-26 20:49:34 +0000

    Minor layout fixes
    Added colon and showed one of the comments.

--- FileHelpers.Examples/Examples/10.QuickStart/40.WriteFileFixed.cs
+++ FileHelpers.Examples/Examples/10.QuickStart/40.WriteFileFixed.cs
@@ -18,7 +18,7 @@ namespace ExamplesFx
 
         //-> FileOut: Output.txt
 
-        // You define the record mapping class
+        // -> You define the Record Mapping class:
 
         //-> File:RecordClass.cs
         [FixedLengthRecord()]
@@ -38,7 +38,7 @@ namespace ExamplesFx
 
         //-> /File
 
-        //-> Now just create some records and write them with the Engine
+        //-> Now just create some records and write them with the Engine:
 
         public override void Run()
         {
