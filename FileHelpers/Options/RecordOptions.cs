5d37faf9a53b520d6e2d214a19da1a51260907a6
Matthias Cavigelli <cavigelli@gmail.com>
2016-10-28 21:51:27 +0200

    Fixed two compiler warnings

--- FileHelpers/Options/RecordOptions.cs
+++ FileHelpers/Options/RecordOptions.cs
@@ -283,7 +283,7 @@ namespace FileHelpers.Options
         }
 
         /// <summary>
-        /// Allows to get an object[] with the values of the fields in the <param name="record"></param>
+        /// Allows to get an object[] with the values of the fields in the <paramref name="record"/>
         /// </summary>
         /// <param name="record">The record that will be transformed to object[]</param>
         /// <returns>The object[] with the values of the fields in the current record</returns>
