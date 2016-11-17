f18e36c9c53e7a96a32ef484ab1eed3f38382672
Marcos Meli <marcos@filehelpers.com>
2016-11-16 23:06:25 -0300

    Some enhace to online wizard

--- OnlineWizard/OnlineWizard/Scripts/CompareTools.js
+++ OnlineWizard/OnlineWizard/Scripts/CompareTools.js
@@ -31,8 +31,8 @@ $(document).ready(function () {
         var res = new Array();
         var linesA = readLines("#txtSetA");
         var linesB = readLines("#txtSetB");
-        for (var _i = 0; _i < linesA.length; _i++) {
-            var lineA = linesA[_i];
+        for (var _i = 0, linesA_1 = linesA; _i < linesA_1.length; _i++) {
+            var lineA = linesA_1[_i];
             if (unique) {
                 if (duplicateCheck[lineA] === true)
                     continue;
@@ -41,8 +41,8 @@ $(document).ready(function () {
             }
             res.push(lineA);
         }
-        for (var _a = 0; _a < linesB.length; _a++) {
-            var lineB = linesB[_a];
+        for (var _a = 0, linesB_1 = linesB; _a < linesB_1.length; _a++) {
+            var lineB = linesB_1[_a];
             if (unique) {
                 if (duplicateCheck[lineB] === true)
                     continue;
@@ -59,14 +59,14 @@ $(document).ready(function () {
         var res = new Array();
         var linesA = readLines("#txtSetA");
         var linesB = readLines("#txtSetB");
-        for (var _i = 0; _i < linesA.length; _i++) {
-            var lineA = linesA[_i];
+        for (var _i = 0, linesA_2 = linesA; _i < linesA_2.length; _i++) {
+            var lineA = linesA_2[_i];
             if (hashA[lineA] === true)
                 continue;
             hashA[lineA] = true;
         }
-        for (var _a = 0; _a < linesB.length; _a++) {
-            var lineB = linesB[_a];
+        for (var _a = 0, linesB_2 = linesB; _a < linesB_2.length; _a++) {
+            var lineB = linesB_2[_a];
             if (hashA[lineB] === true) {
                 if (unique) {
                     if (duplicateCheck[lineB] === true)
@@ -85,14 +85,14 @@ $(document).ready(function () {
         var res = new Array();
         var linesA = readLines("#txtSetA");
         var linesB = readLines("#txtSetB");
-        for (var _i = 0; _i < linesB.length; _i++) {
-            var lineB = linesB[_i];
+        for (var _i = 0, linesB_3 = linesB; _i < linesB_3.length; _i++) {
+            var lineB = linesB_3[_i];
             if (hashB[lineB] === true)
                 continue;
             hashB[lineB] = true;
         }
-        for (var _a = 0; _a < linesA.length; _a++) {
-            var lineA = linesA[_a];
+        for (var _a = 0, linesA_3 = linesA; _a < linesA_3.length; _a++) {
+            var lineA = linesA_3[_a];
             if (hashB[lineA] === true) {
                 continue;
             }
@@ -112,14 +112,14 @@ $(document).ready(function () {
         var res = new Array();
         var linesB = readLines("#txtSetA");
         var linesA = readLines("#txtSetB");
-        for (var _i = 0; _i < linesB.length; _i++) {
-            var lineB = linesB[_i];
+        for (var _i = 0, linesB_4 = linesB; _i < linesB_4.length; _i++) {
+            var lineB = linesB_4[_i];
             if (hashB[lineB] === true)
                 continue;
             hashB[lineB] = true;
         }
-        for (var _a = 0; _a < linesA.length; _a++) {
-            var lineA = linesA[_a];
+        for (var _a = 0, linesA_4 = linesA; _a < linesA_4.length; _a++) {
+            var lineA = linesA_4[_a];
             if (hashB[lineA] === true) {
                 continue;
             }
