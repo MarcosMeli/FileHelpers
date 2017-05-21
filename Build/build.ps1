d48696a4e84eb6a630f05a47f1989c3e1dfad62a
Matthias Cavigelli <cavigelli@gmail.com>
2017-05-21 14:03:04 +0200

    Making build with powershell
    Makes it better debugable with ISE and is easier to invoke from appveyor

---
+++ Build/build.ps1
@@ -0,0 +1,12 @@
+SET EnableNuGetPackageRestore=true
+.nuget\NuGet.exe install .nuget\packages.config -OutputDirectory ..\packages
+.nuget\NuGet.exe restore ..\FileHelpers.sln
+
+Import-Module '..\packages\psake.*\tools\psake.psm1';
+invoke-psake default.ps1 compile;
+
+if ($LastExitCode -ne 0)
+{
+  write-host "ERROR: $LastExitCode" -fore RED;
+  exit $LastExitCode
+}
