ea113b3784d03948c97893dd4de79349bc6cb61a
Marc Lopez <marcrainier.lopez@parallon.com>
2017-05-31 12:57:12 -0400

    Removed commented code in ExcelNPOIStorage.Program
    (cherry picked from commit 3472c4dba83bc2ea6f23eb3d55853b9b8a33f2a8)

--- FileHelpers.ExcelNPOIStorage/Test/Program.cs
+++ FileHelpers.ExcelNPOIStorage/Test/Program.cs
@@ -14,13 +14,6 @@ namespace OurTest
     {
         private static void Main(string[] args)
         {
-            /*var provider = new ExcelStorage(typeof(RaRecord)) {
-                StartRow = 2,
-                StartColumn = 1,
-                SheetName = "Sheet2",
-                FileName = "test.xlsx"
-            };*/
-
             //Dynamic Records
             var cb = new DelimitedClassBuilder("Customer", "|")
             {
@@ -125,25 +118,6 @@ namespace OurTest
             
             var res = (RaRecord[])provider.ExtractRecords();
         }
-
-//        static void Main(string[] args)
-//        {
-//            var provider = new ExcelStorage(typeof(RaRecord));
-//
-//            provider.StartRow = 2;
-//            //provider.StartColumn = 2;
-//
-//            provider.FileName = "test.new.xlsx";
-//            provider.TemplateFile = "test.xlsx";
-//            provider.SheetName = "TEST";
-//            var records = new RaRecord[] {
-//                new RaRecord{Level = 1, Name = "Bavo", Project = "Eandis"}, 
-//                new RaRecord{Name = "Michiel", Project = "Eandis", Startdate = DateTime.Now}, 
-//                new RaRecord{Name = "", Startdate = DateTime.Now.AddDays(10)}, 
-//            };
-//
-//            provider.InsertRecords(records);
-//        }
     }
     
     [DelimitedRecord("|")]
