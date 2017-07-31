5eeea5aa84b549da039c7209fe07c05cfd5a2787
Matthias Cavigelli <cavigelli@gmail.com>
2017-07-31 22:11:39 +0200

    Formatting of build script

--- Build/default.ps1
+++ Build/default.ps1
@@ -17,42 +17,6 @@ task version {
     Update-NuGetVersion '..\Build\NuGet\FileHelpers.nuspec' $VisibleVersion
 }
 
-function Update-AssemblyInfoFile ([string] $filename, [string] $assemblyVersionNumber, [string] $fileVersionNumber, [string] $informationalVersionNumber)
-{
-    $assemblyVersionPattern = 'AssemblyVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)'
-    $fileVersionPattern = 'AssemblyFileVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)'
-    $informationalVersionPattern = 'AssemblyInformationalVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)'
-    $assemblyVersion = 'AssemblyVersion("' + $assemblyVersionNumber + '")';
-    $fileVersion = 'AssemblyFileVersion("' + $fileVersionNumber + '")';
-    $informationalVersion = 'AssemblyInformationalVersion("' + $informationalVersionNumber + '")';
-    
-        Write-Host $filename
-        $filename + ' -> ' + $assemblyVersionNumber
-    
-        (Get-Content $filename) | ForEach-Object {
-            % {$_ -replace $assemblyVersionPattern, $assemblyVersion } |
-            % {$_ -replace $fileVersionPattern, $fileVersion } |
-            % {$_ -replace $informationalVersionPattern, $informationalVersion }
-        } | Set-Content $filename
-}
-
-function Update-NuGetVersion ([string] $filename, [string] $versionNumber)
-{
-    $versionPattern = '\<version\>[0-9]+(\.([0-9]+|\*)){1,3}\<\/version\>'
-    $version = '<version>' + $versionNumber + '</version>';
-
-    $dependenceVersionPattern = 'id="FileHelpers" version=\"[0-9]+(\.([0-9]+|\*)){1,3}\"'
-    $dependenceVersion = 'id="FileHelpers" version="' + $versionNumber + '"';
-    
-    Write-Host $filename
-    $filename + ' -> ' + $assemblyVersionNumber
-    
-        (Get-Content $filename) | ForEach-Object {
-            % {$_ -replace $versionPattern, $version } |
-            % {$_ -replace $dependenceVersionPattern, $dependenceVersion }
-        } | Set-Content $filename
-}
-
 task common -depends version {
      Delete-Make-Directory ..\$config
      Delete-Make-Directory "..\Output"
@@ -76,17 +40,17 @@ task compile -depends common {
 task compiledebug -depends common {
     $config = "Debug"
     "Compiling Debug"
-    
+
     Compile-Sln "..\FileHelpers.sln" "4.5"
 }
 
 task docs -depends compile {
     "Documenting"
-    
+
     exec { msbuild FileHelpers.shfbproj /p:Configuration=$config /nologo }
-    
+
     Make-Directory ..\$config\Docs
-    
+
     copy ..\Help\FileHelpers.chm ..\$config\Docs\FilHelpers.chm
 }
 
@@ -109,38 +73,37 @@ task test -depends compile{
 
 function Delete-Make-Directory($path)
 {
-  Delete-Directory $path
-  Make-Directory $path
+    Delete-Directory $path
+    Make-Directory $path
 }
 
 function Delete-Directory($path)
 {
-  rd $path -recurse -force -ErrorAction SilentlyContinue | out-null
+    rd $path -recurse -force -ErrorAction SilentlyContinue | out-null
 }
 
 function Make-Directory($path)
 {
-  md $path -ErrorAction SilentlyContinue | out-null
+    md $path -ErrorAction SilentlyContinue | out-null
 }
 
 function Compile-Sln($path, $targetFramework)
 {
-     & 'C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe' $path /p:TargetFrameworkVersion=v$targetFramework /t:rebuild /p:Configuration=$config /nologo /verbosity:minimal
+    & 'C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe' $path /p:TargetFrameworkVersion=v$targetFramework /t:rebuild /p:Configuration=$config /nologo /verbosity:minimal
 }
 
 function Compile-Sln-With-Deploy($path, $targetFramework, $deploy)
 {
-   Compile-Sln $path $targetFramework
-   $deployDir = "..\" + $config + "\" + $deploy 
-   Make-Directory $deployDir
- 
-   $fromDir = "..\" + $config + "\Bin"
-   $fromFiles = $fromDir + "\*.*"
- 
-   copy $fromFiles $deployDir
+    Compile-Sln $path $targetFramework
+    $deployDir = "..\" + $config + "\" + $deploy 
+    Make-Directory $deployDir
 
-   Delete-Directory $fromDir
-  
+    $fromDir = "..\" + $config + "\Bin"
+    $fromFiles = $fromDir + "\*.*"
+
+    copy $fromFiles $deployDir
+
+    Delete-Directory $fromDir
 }
 
 function Get-AssemblyInformationalVersion($path)
@@ -167,9 +130,45 @@ function Update-AssemblyInformationalVersion
 function Create-Zip($sourcePath, $destinationFile)
 {
     cd $package_dir\SharpZipLib.*\lib\20\
-    
+
     Add-Type -Path ICSharpCode.SharpZipLib.dll
 
     $zip = New-Object ICSharpCode.SharpZipLib.Zip.FastZip
     $zip.CreateZip("$destinationFile", "$sourcePath", $true, $null)
+}
+
+function Update-NuGetVersion ([string] $filename, [string] $versionNumber)
+{
+    $versionPattern = '\<version\>[0-9]+(\.([0-9]+|\*)){1,3}\<\/version\>'
+    $version = '<version>' + $versionNumber + '</version>';
+
+    $dependenceVersionPattern = 'id="FileHelpers" version=\"[0-9]+(\.([0-9]+|\*)){1,3}\"'
+    $dependenceVersion = 'id="FileHelpers" version="' + $versionNumber + '"';
+
+    Write-Host $filename
+    $filename + ' -> ' + $assemblyVersionNumber
+
+    (Get-Content $filename) | ForEach-Object {
+         % {$_ -replace $versionPattern, $version } |
+         % {$_ -replace $dependenceVersionPattern, $dependenceVersion }
+    } | Set-Content $filename
+}
+
+function Update-AssemblyInfoFile ([string] $filename, [string] $assemblyVersionNumber, [string] $fileVersionNumber, [string] $informationalVersionNumber)
+{
+    $assemblyVersionPattern = 'AssemblyVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)'
+    $fileVersionPattern = 'AssemblyFileVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)'
+    $informationalVersionPattern = 'AssemblyInformationalVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)'
+    $assemblyVersion = 'AssemblyVersion("' + $assemblyVersionNumber + '")';
+    $fileVersion = 'AssemblyFileVersion("' + $fileVersionNumber + '")';
+    $informationalVersion = 'AssemblyInformationalVersion("' + $informationalVersionNumber + '")';
+
+    Write-Host $filename
+    $filename + ' -> ' + $assemblyVersionNumber
+
+    (Get-Content $filename) | ForEach-Object {
+        % {$_ -replace $assemblyVersionPattern, $assemblyVersion } |
+        % {$_ -replace $fileVersionPattern, $fileVersion } |
+        % {$_ -replace $informationalVersionPattern, $informationalVersion }
+    } | Set-Content $filename
 }
