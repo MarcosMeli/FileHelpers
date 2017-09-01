01fe67fcb5c34c125a381d0a754cebea9111a038
Matthias Cavigelli <cavigelli@gmail.com>
2017-09-01 20:57:45 +0200

    Removed bat build file
    PowerShell should be sufficient by now

--- Build/Build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-RMDIR ..\packages\psake.4.3.2 /s /q
-SET EnableNuGetPackageRestore=true
-.nuget\NuGet.exe install .nuget\packages.config -OutputDirectory ..\packages
-.nuget\NuGet.exe restore ..\FileHelpers.sln
-cd ..
-powershell.exe -NoProfile -ExecutionPolicy unrestricted -Command "& {Import-Module '.\packages\psake.*\tools\psake.psm1'; invoke-psake .\Build\default.ps1 test; if ($LastExitCode -ne 0) {write-host "ERROR: $LastExitCode" -fore RED; exit $lastexitcode} }"
