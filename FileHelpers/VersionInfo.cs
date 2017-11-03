dadc2ccc6febb088409e9c8ff333c8f821d75a73
Matthias Cavigelli <cavigelli@gmail.com>
2017-11-03 21:23:39 +0100

    Release 3.2.6

--- FileHelpers/VersionInfo.cs
+++ FileHelpers/VersionInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 // The version of the Library
 [assembly : AssemblyVersion("3.2.6")]
 [assembly : AssemblyFileVersion("3.2.6")]
-[assembly : AssemblyInformationalVersion("3.2.6-alpha")]
+[assembly : AssemblyInformationalVersion("3.2.6")]
 
 [assembly : AssemblyDescription("A free and easy to use .NET library to read/write data from fixed length or delimited records in files, strings or streams")]
 [assembly : AssemblyProduct("FileHelpers  http://www.filehelpers.net")]
