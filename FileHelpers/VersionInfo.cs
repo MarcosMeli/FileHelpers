40e2577ab04061775f5b99070a7299e4c4a73cfb
Matthias Cavigelli <cavigelli@gmail.com>
2018-05-28 21:55:11 +0200

    Updated metadata of .NET Core library

--- FileHelpers/VersionInfo.cs
+++ FileHelpers/VersionInfo.cs
@@ -7,11 +7,11 @@ using System.Runtime.InteropServices;
 [assembly : AssemblyInformationalVersion("3.2.8-alpha")]
 
 [assembly : AssemblyDescription("A free and easy to use .NET library to read/write data from fixed length or delimited records in files, strings or streams")]
-[assembly : AssemblyProduct("FileHelpers  http://www.filehelpers.net")]
+[assembly : AssemblyProduct("FileHelpers http://www.filehelpers.net")]
 
 [assembly : AssemblyCompany("Devoo")]
 [assembly : AssemblyTrademark("FileHelpers")]
-[assembly : AssemblyCopyright("Copyright 2005-2015. Marcos Meli")]
+[assembly : AssemblyCopyright("Copyright 2005-2018. Marcos Meli")]
 [assembly : AssemblyCulture("")]
 [assembly : AssemblyConfiguration("")]
 [assembly : ComVisible(false)]
