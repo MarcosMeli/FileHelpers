3d6dabde42eda4bb310d8be36309baa760c66811
Matthias Cavigelli <cavigelli@gmail.com>
2018-12-13 21:39:20 +0100

    Minor changes (formatting, removing redundant code) of #306

--- FileHelpers/Attributes/TypedRecordAttribute.cs
+++ FileHelpers/Attributes/TypedRecordAttribute.cs
@@ -4,26 +4,22 @@ using System.ComponentModel;
 namespace FileHelpers
 {
     /// <summary>Base class for the record types..</summary>
-    /// <remarks>See the <a href="http://www.filehelpers.net/mustread">complete attributes list</a> for more information and examples of each one.</remarks>
+    /// <remarks>See the <a href="http://www.filehelpers.net/mustread">complete attributes list</a>
+    /// for more information and examples of each one.</remarks>
     [AttributeUsage(AttributeTargets.Class)]
     [EditorBrowsable(EditorBrowsableState.Never)]
-    public abstract class TypedRecordAttribute
-        : Attribute
+    public abstract class TypedRecordAttribute : Attribute
     {
         /// <summary>
-        /// Default culture name used for each properties if no converter is specified otherwise. If null, the default decimal separator (".") will be used.
+        /// Default culture name used for each properties if no converter is specified otherwise.
+        /// If null, the default decimal separator (".") will be used.
         /// </summary>
-        public string DefaultCultureName { get; private set; }
-
-
-        #region "  Constructors  "
+        public string DefaultCultureName { get; }
 
         /// <summary>Abstract class, see inheritors.</summary>
         protected TypedRecordAttribute(string defaultCultureName)
         {
-            this.DefaultCultureName = defaultCultureName;
+            DefaultCultureName = defaultCultureName;
         }
-
-        #endregion
     }
 }
