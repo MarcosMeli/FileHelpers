0967437911a4ff86ee52d14fd84714c9a18dc04e
Matthias Cavigelli <cavigelli@gmail.com>
2019-03-19 21:57:18 +0100

    Version: 3.4.0!

--- FileHelpers/VersionInfo.cs
+++ FileHelpers/VersionInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 // The version of the Library
 [assembly : AssemblyVersion("3.4.0")]
 [assembly : AssemblyFileVersion("3.4.0")]
-[assembly : AssemblyInformationalVersion("3.4.0-beta")]
+[assembly : AssemblyInformationalVersion("3.4.0")]
 
 [assembly : AssemblyDescription("A free and easy to use .NET library to read/write data from fixed length or delimited records in files, strings or streams")]
 [assembly : AssemblyProduct("FileHelpers http://www.filehelpers.net")]
