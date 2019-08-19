781c4c10c7fd31374914cec4d7d0b62411f359e4
Matthias Cavigelli <cavigelli@gmail.com>
2019-08-19 22:19:21 +0200

    Removed unmaintained Demos folder

--- FileHelpers.Demos/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Reflection;
-
-
-[assembly: AssemblyTitle("FileHelpers Sample App")]
-[assembly: AssemblyDelaySign(false)]
-[assembly: AssemblyKeyFile("")]
-[assembly: AssemblyKeyName("")]
