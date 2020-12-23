66098c079c854c18dfcb8360d627b9162d095b59
Matthias Cavigelli <cavigelli@gmail.com>
2020-12-23 15:02:46 +0100

    Removed unused test

--- FileHelpers.Tests/Tests/Common/ChineseSupport.cs
+++ FileHelpers.Tests/Tests/Common/ChineseSupport.cs
@@ -44,25 +44,6 @@ A987654321台北市民權東東路5號           20061008");
             Assert.AreEqual("A123456789", res[0].id);
         }
 
-//
-//		[Test]
-//		public void ReadFileUnicode()
-//		{
-//			engine = new FileHelperEngine(typeof (ChineseTest));
-//
-//			ChineseTest[] res;
-//			engine.Encoding = Encoding.Unicode;
-//			res = (ChineseTest[]) Common.ReadTest(engine, "Good", "ChineseUnicode.txt");
-//
-//			Assert.AreEqual(2, res.Length);
-//			Assert.AreEqual(2, engine.TotalRecords);
-//			Assert.AreEqual(0, engine.ErrorManager.ErrorCount);
-//
-//			Assert.AreEqual(new DateTime(2006, 10, 08), res[0].fecha);
-//			Assert.AreEqual("台北市民權東東路3號           ", res[0].chino);
-//			Assert.AreEqual("A123456789", res[0].id);
-//		}
-
         [FixedLengthRecord(FixedMode.ExactLength)]
         private class ChineseTest
         {
