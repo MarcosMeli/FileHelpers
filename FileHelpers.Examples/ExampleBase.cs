601281747106ca026803c22b7c2f1a0382ebc6fa
Matthias Cavigelli <matthias.cavigelli@atos.net>
2021-03-06 17:13:15 +0100

    Removed ExamplesFx dependency
    Currently no execution and no generation of the documentation.

---
+++ FileHelpers.Examples/ExampleBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ExamplesFx
+{
+    public abstract class ExampleBase
+    {
+        /// <summary>This property allows inheritors to call Console.Method() just like the static Console class.
+        /// This fake console captures the output. The output is used for the documentation generation.
+        /// </summary>
+        protected VirtualConsole Console => new VirtualConsole();
+
+        public abstract void Run();
+    }
+}
---
+++ FileHelpers.Examples/VirtualConsole.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ExamplesFx
+{
+    /// <summary>
+    ///     Idea is copied from
+    ///     <see href="https://github.com/MarcosMeli/ExamplesFx/blob/master/ExamplesFx/ExampleBase.cs" />.
+    /// </summary>
+    public class VirtualConsole
+    {
+        public void WriteLine()
+        {
+            Console.WriteLine();
+        }
+
+        public void WriteLine(string value)
+        {
+            Console.WriteLine(value);
+        }
+
+        public void WriteLine(decimal? value)
+        {
+            Console.WriteLine(value);
+        }
+
+        public void WriteLine(object value)
+        {
+            Console.WriteLine(value);
+        }
+
+        public void WriteLine(string format, params object[] arg)
+        {
+            Console.WriteLine(format, arg);
+        }
+    }
+}
