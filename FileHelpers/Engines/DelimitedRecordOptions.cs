1f60300305c59c1f435519049c0851096b2a1636
Matthias Cavigelli <matthias.cavigelli@atos.net>
2021-04-10 18:11:23 +0200

    Moved options to their engines

---
+++ FileHelpers/Engines/DelimitedRecordOptions.cs
@@ -0,0 +1,31 @@
+namespace FileHelpers.Options
+{
+    /// <summary>
+    /// This class allows you to set some options of the delimited records
+    /// at runtime. With options the library is more flexible.
+    /// </summary>
+    public sealed class DelimitedRecordOptions
+        : RecordOptions
+    {
+        /// <summary>
+        /// This class allows you to set some options of the delimited records
+        /// at runtime. With options the library is more flexible.
+        /// </summary>
+        /// <param name="info">Record information</param>
+        internal DelimitedRecordOptions(IRecordInfo info)
+            : base(info) {}
+
+        /// <summary>
+        /// The delimiter used to identify each field in the data.
+        /// </summary>
+        public string Delimiter
+        {
+            get { return ((DelimitedField) mRecordInfo.Fields[0]).Separator; }
+            set
+            {
+                for (int i = 0; i < mRecordInfo.FieldCount; i++)
+                    ((DelimitedField) mRecordInfo.Fields[i]).Separator = value;
+            }
+        }
+    }
+}
